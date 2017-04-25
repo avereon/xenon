71908c529d3526b86537d9d4d6770d05e0500c96
Mark Soderquist <soderquistmv@ldschurch.org>
2017-04-25 15:29:53 -0600

    Work on creating unit tests

--- source/test/java/com/parallelsymmetry/essence/ProgramTest.java
+++ source/test/java/com/parallelsymmetry/essence/ProgramTest.java
@@ -8,6 +8,9 @@ import org.w3c.dom.Document;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathFactory;
 import java.io.File;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -30,10 +33,28 @@ public class ProgramTest {
 		DocumentBuilder builder = dbf.newDocumentBuilder();
 		Document pom = builder.parse( new File( "pom.xml" ) );
 
+		XPath xpath = XPathFactory.newInstance().newXPath();
+		String groupId = (String)xpath.evaluate( "/project/groupId", pom, XPathConstants.STRING );
+		String artifactId = (String)xpath.evaluate( "/project/artifactId", pom, XPathConstants.STRING );
+		String version = (String)xpath.evaluate( "/project/parent/version", pom, XPathConstants.STRING );
+
+		String name = (String)xpath.evaluate( "/project/name", pom, XPathConstants.STRING );
+		String provider = (String)xpath.evaluate( "/project/organization/name", pom, XPathConstants.STRING );
+		int inception = Integer.parseInt((String)xpath.evaluate( "/project/inceptionYear", pom, XPathConstants.STRING ) );
+
+		String description = (String)xpath.evaluate( "/project/description", pom, XPathConstants.STRING );
+
 		ProductMetadata metadata = program.getMetadata();
 		assertNotNull( metadata );
-		assertThat( metadata.getGroup(), is( "com.parallelsymmetry" ) );
-		assertThat( metadata.getGroup(), is( "essence" ) );
+		assertThat( metadata.getGroup(), is( groupId ) );
+		assertThat( metadata.getArtifact(), is( artifactId ) );
+		assertThat( metadata.getVersion(), is( version ) );
+
+		assertThat( metadata.getName(), is( name ) );
+		assertThat( metadata.getProvider(), is( provider ) );
+		assertThat( metadata.getInception(), is( inception ) );
+
+		assertThat( metadata.getDescription(), is( description ) );
 	}
 
 }
