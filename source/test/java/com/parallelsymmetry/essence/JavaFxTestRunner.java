9850f41bb32c5c57454067f5142f10a161d8ca3b
Mark Soderquist <soderquistmv@ldschurch.org>
2017-04-25 13:58:59 -0600

    Work on creating unit tests

---
+++ source/test/java/com/parallelsymmetry/essence/JavaFxTestRunner.java
@@ -0,0 +1,49 @@
+package com.parallelsymmetry.essence;
+
+import javafx.application.Platform;
+import org.junit.runner.RunWith;
+import org.junit.runners.BlockJUnit4ClassRunner;
+import org.junit.runners.model.InitializationError;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * This runner can be used to run JUnit-Tests on the JavaFx-Thread. This class
+ * can be used as a parameter to the {@link RunWith} annotation. Example:
+ * <p>
+ * <pre>
+ * <code>&#64;RunWith( JavaFxTestRunner.class )
+ * public class SomeUnitTest {
+ *   &#64;Test
+ *   public void testSomeMethod() {
+ *    //...
+ *   }
+ * }</code>
+ * </pre>
+ */
+public class JavaFxTestRunner extends BlockJUnit4ClassRunner {
+
+	/**
+	 * Creates a test runner, that initializes the JavaFx runtime.
+	 *
+	 * @param clazz The class under test.
+	 * @throws InitializationError if the test class is malformed.
+	 */
+	public JavaFxTestRunner( final Class<?> clazz ) throws InitializationError {
+		super( clazz );
+		try {
+			setupJavaFX();
+		} catch( final InterruptedException e ) {
+			throw new InitializationError( "Could not initialize the JavaFx platform." );
+		}
+	}
+
+	private static void setupJavaFX() throws InterruptedException {
+		final CountDownLatch latch = new CountDownLatch( 1 );
+		Platform.startup( () -> {} );
+		//Platform.startup( latch::countDown );
+		latch.countDown();
+		latch.await();
+	}
+
+}
--- source/test/java/com/parallelsymmetry/essence/ProgramTest.java
+++ source/test/java/com/parallelsymmetry/essence/ProgramTest.java
@@ -1,16 +1,39 @@
 package com.parallelsymmetry.essence;
 
-import org.junit.Rule;
+import com.parallelsymmetry.essence.product.ProductMetadata;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.w3c.dom.Document;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertThat;
+
+@RunWith( JavaFxTestRunner.class )
 public class ProgramTest {
 
-	@Rule
-	public JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
+	Program program = new Program();
+
+	@Before
+	public void setup() throws Exception {
+		program.init();
+	}
 
 	@Test
-	public void testStart() {
-		//Application.launch(Program.class, new String[0]);
+	public void testProgramMetadata() throws Exception {
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = dbf.newDocumentBuilder();
+		Document pom = builder.parse( new File( "pom.xml" ) );
+
+		ProductMetadata metadata = program.getMetadata();
+		assertNotNull( metadata );
+		assertThat( metadata.getGroup(), is( "com.parallelsymmetry" ) );
+		assertThat( metadata.getGroup(), is( "essence" ) );
 	}
 
 }
