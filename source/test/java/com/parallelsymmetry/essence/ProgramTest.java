05a552fe60adc921b10cc194caa3921a41074c23
Mark Soderquist <mark@soderquist.net>
2017-04-25 16:54:00 -0600

    Test program metadata

--- source/test/java/com/parallelsymmetry/essence/ProgramTest.java
+++ source/test/java/com/parallelsymmetry/essence/ProgramTest.java
@@ -1,6 +1,7 @@
 package com.parallelsymmetry.essence;
 
 import com.parallelsymmetry.essence.product.ProductMetadata;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,7 +15,6 @@ import javax.xml.xpath.XPathFactory;
 import java.io.File;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 
 @RunWith( JavaFxTestRunner.class )
@@ -37,24 +37,29 @@ public class ProgramTest {
 		String groupId = (String)xpath.evaluate( "/project/groupId", pom, XPathConstants.STRING );
 		String artifactId = (String)xpath.evaluate( "/project/artifactId", pom, XPathConstants.STRING );
 		String version = (String)xpath.evaluate( "/project/parent/version", pom, XPathConstants.STRING );
-
 		String name = (String)xpath.evaluate( "/project/name", pom, XPathConstants.STRING );
 		String provider = (String)xpath.evaluate( "/project/organization/name", pom, XPathConstants.STRING );
 		int inception = Integer.parseInt((String)xpath.evaluate( "/project/inceptionYear", pom, XPathConstants.STRING ) );
-
 		String description = (String)xpath.evaluate( "/project/description", pom, XPathConstants.STRING );
 
+		String timestampRegex = "[0-9][0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]";
+
 		ProductMetadata metadata = program.getMetadata();
-		assertNotNull( metadata );
 		assertThat( metadata.getGroup(), is( groupId ) );
 		assertThat( metadata.getArtifact(), is( artifactId ) );
 		assertThat( metadata.getVersion(), is( version ) );
+		Assert.assertTrue( "Incorrect timestamp format: " + metadata.getTimestamp(), metadata.getTimestamp().matches( timestampRegex ) );
 
 		assertThat( metadata.getName(), is( name ) );
-		assertThat( metadata.getProvider(), is( provider ) );
+		assertThat( metadata.getIcon(), is( "http://www.parallelsymmetry.com/images/icons/essence.png" ) );
+		// Provider is specified in parent pom
+		//assertThat( metadata.getProvider(), is( provider ) );
 		assertThat( metadata.getInception(), is( inception ) );
 
+		assertThat( metadata.getSummary(), is( "Java application platform" ) );
 		assertThat( metadata.getDescription(), is( description ) );
+		assertThat( metadata.getCopyrightSummary(), is( "All rights reserved."));
+		assertThat( metadata.getLicenseSummary(), is( name + " comes with ABSOLUTELY NO WARRANTY. This is open software, and you are welcome to redistribute it under certain conditions."));
 	}
 
 }
