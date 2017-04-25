4145422829c6726ac359b21a81c612bd04cd2c8b
Mark Soderquist <soderquistmv@ldschurch.org>
2017-04-25 10:57:43 -0600

    Work on creating unit tests

---
+++ source/test/java/com/parallelsymmetry/essence/ProgramTest.java
@@ -0,0 +1,16 @@
+package com.parallelsymmetry.essence;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class ProgramTest {
+
+	@Rule
+	public JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
+
+	@Test
+	public void testStart() {
+		//Application.launch(Program.class, new String[0]);
+	}
+
+}
