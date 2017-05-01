81bd2bda508607909ed191e132b1d81a639ae385
Mark Soderquist <soderquistmv@ldschurch.org>
2017-05-01 16:37:08 -0600

    Add lines for the default actions

--- source/main/java/com/parallelsymmetry/essence/ActionLibrary.java
+++ source/main/java/com/parallelsymmetry/essence/ActionLibrary.java
@@ -18,17 +18,55 @@ public class ActionLibrary {
 		this.actions = new ConcurrentHashMap<>();
 		this.icons = icons;
 
+		// Create default actions
 		register( bundle, icons, "file" );
 		register( bundle, icons, "new" );
 		register( bundle, icons, "open" );
 		register( bundle, icons, "save" );
+		//register( bundle, icons, "save-as" );
+		//register( bundle, icons, "copy-as" );
 		register( bundle, icons, "close" );
 		register( bundle, icons, "exit" );
+
 		register( bundle, icons, "edit" );
+		register( bundle, icons, "undo" );
+		register( bundle, icons, "redo" );
+		register( bundle, icons, "cut" );
+		register( bundle, icons, "copy" );
+		register( bundle, icons, "paste" );
+		register( bundle, icons, "delete" );
+		register( bundle, icons, "indent" );
+		register( bundle, icons, "unindent" );
+		register( bundle, icons, "properties" );
+		register( bundle, icons, "settings" );
+
 		register( bundle, icons, "view" );
-		register( bundle, icons, "workarea-new" );
+//		register( bundle, icons, "window-new" );
+//		register( bundle, icons, "tool-new" );
+		//		register( bundle, icons, "view-default" );
+		//		register( bundle, icons, "view-split-horizontal" );
+		//		register( bundle, icons, "view-split-vertical" );
+		//		register( bundle, icons, "view-merge-north" );
+		//		register( bundle, icons, "view-merge-south" );
+		//		register( bundle, icons, "view-merge-east" );
+		//		register( bundle, icons, "view-merge-west" );
+
 		register( bundle, icons, "help" );
+		//		register( bundle, icons, "welcome" );
+		//		register( bundle, icons, "system-properties" );
+		//		register( bundle, icons, "products" );
+		//		register( bundle, icons, "updates" );
 		register( bundle, icons, "about" );
+
+		//		register( bundle, icons, "development" );
+		//		register( bundle, icons, "settings-reset" );
+		//		register( bundle, icons, "workers" );
+		//		register( bundle, icons, "restart" );
+
+		//		register( bundle, icons, "workarea" );
+		//		register( bundle, icons, "workarea-new" );
+		//		register( bundle, icons, "workarea-copy" );
+		//		register( bundle, icons, "workarea-close" );
 	}
 
 	public ActionProxy getAction( String id ) {
