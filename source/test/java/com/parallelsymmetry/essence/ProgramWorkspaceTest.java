6c9009c6da16175bb90ceb0513c3c09686ee1ee2
Mark Soderquist <mark@soderquist.net>
2017-05-24 18:07:12 -0600

    Add default window size test

--- source/test/java/com/parallelsymmetry/essence/ProgramWorkspaceTest.java
+++ source/test/java/com/parallelsymmetry/essence/ProgramWorkspaceTest.java
@@ -2,6 +2,7 @@ package com.parallelsymmetry.essence;
 
 import com.parallelsymmetry.essence.event.ProgramStartedEvent;
 import com.parallelsymmetry.essence.testutil.FxTestCase;
+import javafx.stage.Stage;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
@@ -9,23 +10,30 @@ import static org.junit.Assert.assertThat;
 
 public class ProgramWorkspaceTest extends FxTestCase {
 
-//	@Test
-//	public void testA() {
-//		Assert.assertEquals( "Essence", program.getMetadata().getName() );
-//	}
-//
-//	@Test
-//	public void testB() {
-//		Assert.assertEquals( "Essence", program.getMetadata().getName() );
-//	}
+	//	@Test
+	//	public void testA() {
+	//		Assert.assertEquals( "Essence", program.getMetadata().getName() );
+	//	}
+	//
+	//	@Test
+	//	public void testB() {
+	//		Assert.assertEquals( "Essence", program.getMetadata().getName() );
+	//	}
+
+	@Test
+	public void testWorkspaceDefaultWindowSize() throws Exception {
+		waitForEvent( ProgramStartedEvent.class );
+		Stage stage = program.getWorkspaceManager().getActiveWorkspace().getStage();
+
+		assertThat( stage.getWidth(), is( 960d ) );
+		assertThat( stage.getHeight(), is( 540d ) );
+	}
 
 	@Test
 	public void testWorkspaceWindowTitle() throws Exception {
 		waitForEvent( ProgramStartedEvent.class );
 		String workareaName = program.getWorkspaceManager().getActiveWorkspace().getActiveWorkarea().getName();
 		assertThat( program.getWorkspaceManager().getActiveWorkspace().getStage().getTitle(), is( workareaName + " - " + metadata.getName() ) );
-		System.out.println( "All good in the window title test" );
 	}
-
 
 }
