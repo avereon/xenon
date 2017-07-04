ac903c82a9d4d0ea09e9808a5b315cf03f449b44
Mark Soderquist <mark@soderquist.net>
2017-07-04 07:40:27 -0600

    Refactor UriUtilTest to use hamcrest

--- source/test/java/com/parallelsymmetry/essence/UriUtilTest.java
+++ source/test/java/com/parallelsymmetry/essence/UriUtilTest.java
@@ -1,77 +1,86 @@
 package com.parallelsymmetry.essence;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
 import java.io.File;
 import java.net.URI;
 import java.util.Map;
 
-public class UriUtilTest extends TestCase {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
 
+public class UriUtilTest extends BaseTestCase {
+
+	@Test
 	public void testResolveWithString() throws Exception {
-		assertEquals( new File( "" ).getCanonicalFile().toURI(), UriUtil.resolve( "" ) );
-		assertEquals( new File( "." ).getCanonicalFile().toURI(), UriUtil.resolve( "." ) );
-		assertEquals( new File( "test" ).getCanonicalFile().toURI(), UriUtil.resolve( "test" ) );
-		assertEquals( new File( "/test" ).getCanonicalFile().toURI(), UriUtil.resolve( "/test" ) );
-
-		assertEquals( URI.create( "ssh://localhost" ), UriUtil.resolve( "ssh://localhost" ) );
+		assertThat( UriUtil.resolve( "" ), is( new File( "" ).getCanonicalFile().toURI() ) );
+		assertThat( UriUtil.resolve( "." ), is( new File( "." ).getCanonicalFile().toURI() ) );
+		assertThat( UriUtil.resolve( "test" ), is( new File( "test" ).getCanonicalFile().toURI() ) );
+		assertThat( UriUtil.resolve( "/test" ), is( new File( "/test" ).getCanonicalFile().toURI() ) );
+		assertThat( UriUtil.resolve( "ssh://localhost" ), is( URI.create( "ssh://localhost" ) ) );
+		assertThat( UriUtil.resolve( "program:about" ), is( URI.create( "program:about" ) ) );
 	}
 
+	@Test
 	public void testResolveWithRelativeUri() {
 		URI base = URI.create( "file:/test/folder/" );
 		URI absolute = URI.create( "file:/test/folder/absolute" );
 		URI relative = URI.create( "relative" );
 		URI jar = URI.create( "jar:file:/test/folder%20with%20spaces/file.jar!/path/to/resource" );
 
-		assertEquals( null, UriUtil.resolve( null, null ) );
-		assertEquals( null, UriUtil.resolve( base, null ) );
-		assertEquals( URI.create( "relative" ), UriUtil.resolve( null, relative ) );
-		assertEquals( URI.create( "file:/test/folder/absolute" ), UriUtil.resolve( null, absolute ) );
+		assertThat( UriUtil.resolve( null, null ), is( nullValue() ) );
+		assertThat( UriUtil.resolve( base, null ), is( nullValue() ) );
+		assertThat( UriUtil.resolve( null, relative ), is( URI.create( "relative" ) ) );
+		assertThat( UriUtil.resolve( null, absolute ), is( URI.create( "file:/test/folder/absolute" ) ) );
 
-		assertEquals( URI.create( "file:/test/folder/absolute" ), UriUtil.resolve( base, absolute ) );
-		assertEquals( URI.create( "file:/test/folder/relative" ), UriUtil.resolve( base, relative ) );
-		assertEquals( URI.create( "file:/test/folder/relative" ), UriUtil.resolve( absolute, relative ) );
+		assertThat( UriUtil.resolve( base, absolute ), is( URI.create( "file:/test/folder/absolute" ) ) );
+		assertThat( UriUtil.resolve( base, relative ), is( URI.create( "file:/test/folder/relative" ) ) );
+		assertThat( UriUtil.resolve( absolute, relative ), is( URI.create( "file:/test/folder/relative" ) ) );
 
-		assertEquals( URI.create( "jar:file:/test/folder%20with%20spaces/file.jar!/path/to/relative" ), UriUtil.resolve( jar, relative ) );
+		assertThat( UriUtil.resolve( jar, relative ), is( URI.create( "jar:file:/test/folder%20with%20spaces/file.jar!/path/to/relative" ) ) );
 
 		URI icon = URI.create( "http://www.parallelsymmetry.com/images/icons/escape.png" );
-		assertEquals( URI.create( "http://www.parallelsymmetry.com/images/icons/escape.png" ), UriUtil.resolve( jar, icon ) );
+		assertThat( UriUtil.resolve( jar, icon ), is( icon ) );
 	}
 
+	@Test
 	public void testGetParent() {
 		URI absolute = URI.create( "file:/test/folder/absolute" );
 		URI opaque = URI.create( "jar:" + absolute.toString() );
 		URI doubleOpaque = URI.create( "double:jar:" + absolute.toString() );
 
-		assertEquals( "file:/test/folder/", UriUtil.getParent( absolute ).toString() );
-		assertEquals( "jar:file:/test/folder/", UriUtil.getParent( opaque ).toString() );
-		assertEquals( "double:jar:file:/test/folder/", UriUtil.getParent( doubleOpaque ).toString() );
+		assertThat( UriUtil.getParent( absolute ).toString(), is( "file:/test/folder/" ) );
+		assertThat( UriUtil.getParent( opaque ).toString(), is( "jar:file:/test/folder/" ) );
+		assertThat( UriUtil.getParent( doubleOpaque ).toString(), is( "double:jar:file:/test/folder/" ) );
 	}
 
+	@Test
 	public void testParseQueryWithUri() {
-		assertNull( UriUtil.parseQuery( (URI)null ) );
+		assertThat( UriUtil.parseQuery( (URI)null ), is( nullValue() ) );
 
 		URI uri = URI.create( "test:///path?attr1&attr2" );
 		Map<String, String> parameters = UriUtil.parseQuery( uri );
-		assertEquals( "true", parameters.get( "attr1" ) );
-		assertEquals( "true", parameters.get( "attr2" ) );
+		assertThat( parameters.get( "attr1" ), is( "true" ) );
+		assertThat( parameters.get( "attr2" ), is( "true" ) );
 
 		uri = URI.create( "test:///path?attr1=value1&attr2=value2" );
 		parameters = UriUtil.parseQuery( uri );
-		assertEquals( "value1", parameters.get( "attr1" ) );
-		assertEquals( "value2", parameters.get( "attr2" ) );
+		assertThat( parameters.get( "attr1" ), is( "value1" ) );
+		assertThat( parameters.get( "attr2" ), is( "value2" ) );
 	}
 
+	@Test
 	public void testParseQueryWithString() {
-		assertNull( UriUtil.parseQuery( (String)null ) );
+		assertThat( UriUtil.parseQuery( (String)null ), is( nullValue() ) );
 
 		Map<String, String> parameters = UriUtil.parseQuery( "attr1&attr2" );
-		assertEquals( "true", parameters.get( "attr1" ) );
-		assertEquals( "true", parameters.get( "attr2" ) );
+		assertThat( parameters.get( "attr1" ), is( "true" ) );
+		assertThat( parameters.get( "attr2" ), is( "true" ) );
 
 		parameters = UriUtil.parseQuery( "attr1=value1&attr2=value2" );
-		assertEquals( "value1", parameters.get( "attr1" ) );
-		assertEquals( "value2", parameters.get( "attr2" ) );
+		assertThat( parameters.get( "attr1" ), is( "value1" ) );
+		assertThat( parameters.get( "attr2" ), is( "value2" ) );
 	}
 
 }
