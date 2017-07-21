e0828591e48171eb1c3d43f9247f512d7bc562a9
Mark Soderquist <mark@soderquist.net>
2017-07-20 18:50:15 -0600

    Work on docked views in workpane

--- source/test/java/com/parallelsymmetry/essence/workarea/WorkpaneDockBottomTest.java
+++ source/test/java/com/parallelsymmetry/essence/workarea/WorkpaneDockBottomTest.java
@@ -2,12 +2,9 @@ package com.parallelsymmetry.essence.workarea;
 
 import com.parallelsymmetry.essence.resource.Resource;
 import javafx.geometry.Side;
-import org.hamcrest.Matchers;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
 public class WorkpaneDockBottomTest extends WorkpaneTestCase {
@@ -26,88 +23,88 @@ public class WorkpaneDockBottomTest extends WorkpaneTestCase {
 		WorkpaneView view = tool.getToolView();
 		assertThat( view, is( not( nullValue() ) ) );
 		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_BOTTOM ) );
-		assertThat( view.getEdge( Side.LEFT ), Matchers.is( workpane.getWallEdge( Side.LEFT ) ) );
-		assertThat( view.getEdge( Side.BOTTOM ), Matchers.is( workpane.getWallEdge( Side.BOTTOM ) ) );
-		assertThat( view.getEdge( Side.RIGHT ), Matchers.is( workpane.getWallEdge( Side.RIGHT ) ) );
+		assertThat( view.getEdge( Side.LEFT ), is( workpane.getWallEdge( Side.LEFT ) ) );
+		assertThat( view.getEdge( Side.BOTTOM ), is( workpane.getWallEdge( Side.BOTTOM ) ) );
+		assertThat( view.getEdge( Side.RIGHT ), is( workpane.getWallEdge( Side.RIGHT ) ) );
 	}
 
 	@Test
-	public void testDockBottomInLandscapeModeWithTopAndBottomDocks() throws Exception {
-		//		Resource resource = new Resource( "mock:resource" );
-		//
-		//		MockTool topTool = new MockTool( resource );
-		//		topTool.setPlacement( Workpane.Placement.DOCK_TOP );
-		//
-		//		MockTool bottomTool = new MockTool( resource );
-		//		bottomTool.setPlacement( Workpane.Placement.DOCK_BOTTOM );
-		//
-		//		MockTool tool = new MockTool( resource );
-		//		tool.setPlacement( Workpane.Placement.DOCK_LEFT );
-		//
-		//		// Add the tool
-		//		workpane.setDockMode( Workpane.DockMode.LANDSCAPE );
-		//		workpane.addTool( topTool );
-		//		workpane.addTool( bottomTool );
-		//		workpane.addTool( tool );
-		//
-		//		// Check the view placement
-		//		WorkpaneView view = tool.getToolView();
-		//		assertThat( view, is( not( nullValue() ) ) );
-		//		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_LEFT ) );
-		//		assertThat( view.getEdge( Side.TOP ), is( workpane.getWallEdge( Side.TOP ) ) );
-		//		assertThat( view.getEdge( Side.LEFT ), is( workpane.getWallEdge( Side.LEFT ) ) );
-		//		assertThat( view.getEdge( Side.BOTTOM ), is( workpane.getWallEdge( Side.BOTTOM ) ) );
+	public void testDockBottomInLandscapeModeWithLeftAndRightDocks() throws Exception {
+		Resource resource = new Resource( "mock:resource" );
+
+		MockTool leftTool = new MockTool( resource );
+		leftTool.setPlacement( Workpane.Placement.DOCK_LEFT );
+
+		MockTool rightTool = new MockTool( resource );
+		rightTool.setPlacement( Workpane.Placement.DOCK_RIGHT );
+
+		MockTool tool = new MockTool( resource );
+		tool.setPlacement( Workpane.Placement.DOCK_BOTTOM );
+
+		// Add the tool
+		workpane.setDockMode( Workpane.DockMode.LANDSCAPE );
+		workpane.addTool( leftTool );
+		workpane.addTool( rightTool );
+		workpane.addTool( tool );
+
+		// Check the view placement
+		WorkpaneView view = tool.getToolView();
+		assertThat( view, is( not( nullValue() ) ) );
+		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_BOTTOM ) );
+		assertThat( view.getEdge( Side.LEFT ), is( leftTool.getToolView().getEdge( Side.RIGHT ) ) );
+		assertThat( view.getEdge( Side.BOTTOM ), is( workpane.getWallEdge( Side.BOTTOM ) ) );
+		assertThat( view.getEdge( Side.RIGHT ), is( rightTool.getToolView().getEdge( Side.LEFT ) ) );
+
+		WorkpaneEdge edge = view.getEdge( Side.TOP );
+		assertThat( edge.getEdge( Side.LEFT ), is( leftTool.getToolView().getEdge( Side.RIGHT ) ) );
+		assertThat( edge.getEdge( Side.RIGHT ), is( rightTool.getToolView().getEdge( Side.LEFT ) ) );
 	}
 
 	@Test
 	public void testDockBottomInPortraitMode() throws Exception {
-		//		Resource resource = new Resource( "mock:resource" );
-		//		MockTool tool = new MockTool( resource );
-		//		tool.setPlacement( Workpane.Placement.DOCK_LEFT );
-		//
-		//		// Add the tool
-		//		workpane.setDockMode( Workpane.DockMode.PORTRAIT );
-		//		workpane.addTool( tool );
-		//
-		//		// Check the view placement
-		//		WorkpaneView view = tool.getToolView();
-		//		assertThat( view, is( not( nullValue() ) ) );
-		//		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_LEFT ) );
-		//		assertThat( view.getEdge( Side.TOP ), is( workpane.getWallEdge( Side.TOP ) ) );
-		//		assertThat( view.getEdge( Side.LEFT ), is( workpane.getWallEdge( Side.LEFT ) ) );
-		//		assertThat( view.getEdge( Side.BOTTOM ), is( workpane.getWallEdge( Side.BOTTOM ) ) );
+		Resource resource = new Resource( "mock:resource" );
+		MockTool tool = new MockTool( resource );
+		tool.setPlacement( Workpane.Placement.DOCK_BOTTOM );
+
+		// Add the tool
+		workpane.setDockMode( Workpane.DockMode.PORTRAIT );
+		workpane.addTool( tool );
+
+		// Check the view placement
+		WorkpaneView view = tool.getToolView();
+		assertThat( view, is( not( nullValue() ) ) );
+		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_BOTTOM ) );
+		assertThat( view.getEdge( Side.LEFT ), is( workpane.getWallEdge( Side.LEFT ) ) );
+		assertThat( view.getEdge( Side.BOTTOM ), is( workpane.getWallEdge( Side.BOTTOM ) ) );
+		assertThat( view.getEdge( Side.RIGHT ), is( workpane.getWallEdge( Side.RIGHT ) ) );
 	}
 
 	@Test
-	public void testDockBottomInPortraitModeWithTopAndBottomDocks() throws Exception {
-		//		Resource resource = new Resource( "mock:resource" );
-		//
-		//		MockTool topTool = new MockTool( resource );
-		//		topTool.setPlacement( Workpane.Placement.DOCK_TOP );
-		//
-		//		MockTool bottomTool = new MockTool( resource );
-		//		bottomTool.setPlacement( Workpane.Placement.DOCK_BOTTOM );
-		//
-		//		MockTool tool = new MockTool( resource );
-		//		tool.setPlacement( Workpane.Placement.DOCK_LEFT );
-		//
-		//		// Add the tool
-		//		workpane.setDockMode( Workpane.DockMode.PORTRAIT );
-		//		workpane.addTool( topTool );
-		//		workpane.addTool( bottomTool );
-		//		workpane.addTool( tool );
-		//
-		//		// Check the view placement
-		//		WorkpaneView view = tool.getToolView();
-		//		assertThat( view, is( not( nullValue() ) ) );
-		//		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_LEFT ) );
-		//		assertThat( view.getEdge( Side.TOP ), is( topTool.getToolView().getEdge( Side.BOTTOM ) ) );
-		//		assertThat( view.getEdge( Side.LEFT ), is( workpane.getWallEdge( Side.LEFT ) ) );
-		//		assertThat( view.getEdge( Side.BOTTOM ), is( bottomTool.getToolView().getEdge( Side.TOP ) ) );
-		//
-		//		WorkpaneEdge edge = view.getEdge( Side.RIGHT );
-		//		assertThat( edge.getEdge( Side.TOP ), is( topTool.getToolView().getEdge( Side.BOTTOM ) ) );
-		//		assertThat( edge.getEdge( Side.BOTTOM ), is( bottomTool.getToolView().getEdge( Side.TOP ) ) );
+	public void testDockBottomInPortraitModeWithLeftAndRightDocks() throws Exception {
+		Resource resource = new Resource( "mock:resource" );
+
+		MockTool leftTool = new MockTool( resource );
+		leftTool.setPlacement( Workpane.Placement.DOCK_LEFT );
+
+		MockTool rightTool = new MockTool( resource );
+		rightTool.setPlacement( Workpane.Placement.DOCK_RIGHT );
+
+		MockTool tool = new MockTool( resource );
+		tool.setPlacement( Workpane.Placement.DOCK_BOTTOM );
+
+		// Add the tool
+		workpane.setDockMode( Workpane.DockMode.PORTRAIT );
+		workpane.addTool( leftTool );
+		workpane.addTool( rightTool );
+		workpane.addTool( tool );
+
+		// Check the view placement
+		WorkpaneView view = tool.getToolView();
+		assertThat( view, is( not( nullValue() ) ) );
+		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_BOTTOM ) );
+		assertThat( view.getEdge( Side.LEFT ), is( workpane.getWallEdge( Side.LEFT ) ) );
+		assertThat( view.getEdge( Side.BOTTOM ), is( workpane.getWallEdge( Side.BOTTOM ) ) );
+		assertThat( view.getEdge( Side.RIGHT ), is( workpane.getWallEdge( Side.RIGHT ) ) );
 	}
 
 }
--- source/test/java/com/parallelsymmetry/essence/workarea/WorkpaneDockTopTest.java
+++ source/test/java/com/parallelsymmetry/essence/workarea/WorkpaneDockTopTest.java
@@ -30,81 +30,81 @@ public class WorkpaneDockTopTest extends WorkpaneTestCase {
 
 	@Test
 	public void testDockTopInLandscapeModeWithTopAndBottomDocks() throws Exception {
-		//		Resource resource = new Resource( "mock:resource" );
-		//
-		//		MockTool topTool = new MockTool( resource );
-		//		topTool.setPlacement( Workpane.Placement.DOCK_TOP );
-		//
-		//		MockTool bottomTool = new MockTool( resource );
-		//		bottomTool.setPlacement( Workpane.Placement.DOCK_BOTTOM );
-		//
-		//		MockTool tool = new MockTool( resource );
-		//		tool.setPlacement( Workpane.Placement.DOCK_LEFT );
-		//
-		//		// Add the tool
-		//		workpane.setDockMode( Workpane.DockMode.LANDSCAPE );
-		//		workpane.addTool( topTool );
-		//		workpane.addTool( bottomTool );
-		//		workpane.addTool( tool );
-		//
-		//		// Check the view placement
-		//		WorkpaneView view = tool.getToolView();
-		//		assertThat( view, is( not( nullValue() ) ) );
-		//		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_LEFT ) );
-		//		assertThat( view.getEdge( Side.TOP ), is( workpane.getWallEdge( Side.TOP ) ) );
-		//		assertThat( view.getEdge( Side.LEFT ), is( workpane.getWallEdge( Side.LEFT ) ) );
-		//		assertThat( view.getEdge( Side.BOTTOM ), is( workpane.getWallEdge( Side.BOTTOM ) ) );
+		Resource resource = new Resource( "mock:resource" );
+
+		MockTool leftTool = new MockTool( resource );
+		leftTool.setPlacement( Workpane.Placement.DOCK_LEFT );
+
+		MockTool rightTool = new MockTool( resource );
+		rightTool.setPlacement( Workpane.Placement.DOCK_RIGHT );
+
+		MockTool tool = new MockTool( resource );
+		tool.setPlacement( Workpane.Placement.DOCK_TOP );
+
+		// Add the tool
+		workpane.setDockMode( Workpane.DockMode.LANDSCAPE );
+		workpane.addTool( leftTool );
+		workpane.addTool( rightTool );
+		workpane.addTool( tool );
+
+		// Check the view placement
+		WorkpaneView view = tool.getToolView();
+		assertThat( view, is( not( nullValue() ) ) );
+		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_TOP ) );
+		assertThat( view.getEdge( Side.LEFT ), is( leftTool.getToolView().getEdge( Side.RIGHT ) ) );
+		assertThat( view.getEdge( Side.TOP ), is( workpane.getWallEdge( Side.TOP ) ) );
+		assertThat( view.getEdge( Side.RIGHT ), is( rightTool.getToolView().getEdge( Side.LEFT ) ) );
+
+		WorkpaneEdge edge = view.getEdge( Side.BOTTOM );
+		assertThat( edge.getEdge( Side.LEFT ), is( leftTool.getToolView().getEdge( Side.RIGHT ) ) );
+		assertThat( edge.getEdge( Side.RIGHT ), is( rightTool.getToolView().getEdge( Side.LEFT ) ) );
 	}
 
 	@Test
 	public void testDockTopInPortraitMode() throws Exception {
-		//		Resource resource = new Resource( "mock:resource" );
-		//		MockTool tool = new MockTool( resource );
-		//		tool.setPlacement( Workpane.Placement.DOCK_LEFT );
-		//
-		//		// Add the tool
-		//		workpane.setDockMode( Workpane.DockMode.PORTRAIT );
-		//		workpane.addTool( tool );
-		//
-		//		// Check the view placement
-		//		WorkpaneView view = tool.getToolView();
-		//		assertThat( view, is( not( nullValue() ) ) );
-		//		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_LEFT ) );
-		//		assertThat( view.getEdge( Side.TOP ), is( workpane.getWallEdge( Side.TOP ) ) );
-		//		assertThat( view.getEdge( Side.LEFT ), is( workpane.getWallEdge( Side.LEFT ) ) );
-		//		assertThat( view.getEdge( Side.BOTTOM ), is( workpane.getWallEdge( Side.BOTTOM ) ) );
+		Resource resource = new Resource( "mock:resource" );
+		MockTool tool = new MockTool( resource );
+		tool.setPlacement( Workpane.Placement.DOCK_TOP );
+
+		// Add the tool
+		workpane.setDockMode( Workpane.DockMode.PORTRAIT );
+		workpane.addTool( tool );
+
+		// Check the view placement
+		WorkpaneView view = tool.getToolView();
+		assertThat( view, is( not( nullValue() ) ) );
+		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_TOP ) );
+		assertThat( view.getEdge( Side.LEFT ), is( workpane.getWallEdge( Side.LEFT ) ) );
+		assertThat( view.getEdge( Side.TOP ), is( workpane.getWallEdge( Side.TOP ) ) );
+		assertThat( view.getEdge( Side.RIGHT ), is( workpane.getWallEdge( Side.RIGHT ) ) );
 	}
 
 	@Test
 	public void testDockTopInPortraitModeWithTopAndBottomDocks() throws Exception {
-		//		Resource resource = new Resource( "mock:resource" );
-		//
-		//		MockTool topTool = new MockTool( resource );
-		//		topTool.setPlacement( Workpane.Placement.DOCK_TOP );
-		//
-		//		MockTool bottomTool = new MockTool( resource );
-		//		bottomTool.setPlacement( Workpane.Placement.DOCK_BOTTOM );
-		//
-		//		MockTool tool = new MockTool( resource );
-		//		tool.setPlacement( Workpane.Placement.DOCK_LEFT );
-		//
-		//		// Add the tool
-		//		workpane.setDockMode( Workpane.DockMode.PORTRAIT );
-		//		workpane.addTool( topTool );
-		//		workpane.addTool( bottomTool );
-		//		workpane.addTool( tool );
-		//
-		//		// Check the view placement
-		//		WorkpaneView view = tool.getToolView();
-		//		assertThat( view, is( not( nullValue() ) ) );
-		//		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_LEFT ) );
-		//		assertThat( view.getEdge( Side.TOP ), is( topTool.getToolView().getEdge( Side.BOTTOM ) ) );
-		//		assertThat( view.getEdge( Side.LEFT ), is( workpane.getWallEdge( Side.LEFT ) ) );
-		//		assertThat( view.getEdge( Side.BOTTOM ), is( bottomTool.getToolView().getEdge( Side.TOP ) ) );
-		//
-		//		WorkpaneEdge edge = view.getEdge( Side.RIGHT );
-		//		assertThat( edge.getEdge( Side.TOP ), is( topTool.getToolView().getEdge( Side.BOTTOM ) ) );
-		//		assertThat( edge.getEdge( Side.BOTTOM ), is( bottomTool.getToolView().getEdge( Side.TOP ) ) );
+		Resource resource = new Resource( "mock:resource" );
+
+		MockTool leftTool = new MockTool( resource );
+		leftTool.setPlacement( Workpane.Placement.DOCK_LEFT );
+
+		MockTool rightTool = new MockTool( resource );
+		rightTool.setPlacement( Workpane.Placement.DOCK_RIGHT );
+
+		MockTool tool = new MockTool( resource );
+		tool.setPlacement( Workpane.Placement.DOCK_TOP );
+
+		// Add the tool
+		workpane.setDockMode( Workpane.DockMode.PORTRAIT );
+		workpane.addTool( leftTool );
+		workpane.addTool( rightTool );
+		workpane.addTool( tool );
+
+		// Check the view placement
+		WorkpaneView view = tool.getToolView();
+		assertThat( view, is( not( nullValue() ) ) );
+		assertThat( view.getPlacement(), is( Workpane.Placement.DOCK_TOP ) );
+		assertThat( view.getEdge( Side.LEFT ), is( workpane.getWallEdge( Side.LEFT ) ) );
+		assertThat( view.getEdge( Side.TOP ), is( workpane.getWallEdge( Side.TOP ) ) );
+		assertThat( view.getEdge( Side.RIGHT ), is( workpane.getWallEdge( Side.RIGHT ) ) );
 	}
 
 }
