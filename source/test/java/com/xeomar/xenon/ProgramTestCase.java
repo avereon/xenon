80613adb9f08ef490ac64657cebf14cecc73552c
Mark Soderquist <mark@soderquist.net>
2017-08-09 20:30:24 -0600

    Commit my work tonight

--- source/test/java/com/xeomar/xenon/ProgramTestCase.java
+++ source/test/java/com/xeomar/xenon/ProgramTestCase.java
@@ -2,6 +2,9 @@ package com.xeomar.xenon;
 
 import org.junit.Before;
 
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
 public class ProgramTestCase extends BaseTestCase {
 
 	protected Program program;
@@ -14,6 +17,7 @@ public class ProgramTestCase extends BaseTestCase {
 		System.setProperty( ProgramParameter.EXECMODE, ProgramParameter.EXECMODE_TEST );
 
 		program = new Program();
+		assertThat( program, not( is( nullValue() )));
 		program.init();
 	}
 
