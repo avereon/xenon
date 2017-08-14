e7f8221126baa6eeb4825136f562d96c9b96d73a
Mark Soderquist <mark@soderquist.net>
2017-08-13 21:22:30 -0600

    Increase event wait watch time for slower computers

--- source/test/java/com/xeomar/xenon/ProgramWatcher.java
+++ source/test/java/com/xeomar/xenon/ProgramWatcher.java
@@ -6,7 +6,7 @@ import java.util.concurrent.TimeoutException;
 
 public class ProgramWatcher implements ProgramEventListener {
 
-	private static final long DEFAULT_WAIT_TIMEOUT = 2000;
+	private static final long DEFAULT_WAIT_TIMEOUT = 5000;
 
 	private Map<Class<? extends ProgramEvent>, ProgramEvent> events = new ConcurrentHashMap<>();
 
