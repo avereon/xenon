724c625f34c87b58c563e99fe0c651d413142a41
Mark Soderquist <soderquistmv@ldschurch.org>
2017-08-24 07:57:13 -0600

    The welcome tool should maximize when activated

--- source/main/java/com/xeomar/xenon/tool/WelcomeTool.java
+++ source/main/java/com/xeomar/xenon/tool/WelcomeTool.java
@@ -5,14 +5,20 @@ import com.xeomar.xenon.Program;
 import com.xeomar.xenon.UiFactory;
 import com.xeomar.xenon.product.Product;
 import com.xeomar.xenon.resource.Resource;
+import com.xeomar.xenon.workarea.Workpane;
+import com.xeomar.xenon.worktool.ToolException;
 import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.shape.Ellipse;
 import javafx.scene.text.Font;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class WelcomeTool extends ProductTool {
+
+	private static final Logger log = LoggerFactory.getLogger( WelcomeTool.class );
 
 	private static final double PAD = 2 * UiFactory.PAD;
 
@@ -40,6 +46,40 @@ public class WelcomeTool extends ProductTool {
 		AnchorPane.setLeftAnchor( label, 0.0 );
 
 		getChildren().addAll( anchorPane );
+	}
+
+	@Override
+	protected void allocate() throws ToolException {
+		log.info( "Tool allocate" );
+	}
+
+	@Override
+	protected void display() throws ToolException {
+		log.info( "Tool display" );
+		Workpane workpane = getWorkpane();
+		if( workpane != null  ) workpane.setMaximizedView( getToolView() );
+	}
+
+	@Override
+	protected void activate() throws ToolException {
+		log.info( "Tool activate" );
+	}
+
+	@Override
+	protected void deactivate() throws ToolException {
+		log.info( "Tool deactivate" );
+	}
+
+	@Override
+	protected void conceal() throws ToolException {
+		log.info( "Tool conceal" );
+		Workpane workpane = getWorkpane();
+		if( workpane != null && workpane.getMaximizedView() == getToolView() ) workpane.setMaximizedView( null );
+	}
+
+	@Override
+	protected void deallocate() throws ToolException {
+		log.info( "Tool deallocate" );
 	}
 
 }
