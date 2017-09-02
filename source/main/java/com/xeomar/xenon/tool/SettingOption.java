7e6ae945c38db1be4bd6feb0637add16c920098b
Mark Soderquist <mark@soderquist.net>
2017-09-01 20:22:54 -0600

    Add setting option class

---
+++ source/main/java/com/xeomar/xenon/tool/SettingOption.java
@@ -0,0 +1,43 @@
+package com.xeomar.xenon.tool;
+
+import com.xeomar.xenon.node.Node;
+
+public class SettingOption extends Node {
+
+	static final String KEY = "key";
+
+	static final String NAME = "name";
+
+	static final String VALUE = "value";
+
+	public SettingOption( String key, String name, String value ) {
+		if( key == null || name == null || value == null ) throw new IllegalArgumentException( "Key cannot be null: " + key + ", " + name + ", " + value );
+
+		super.setValue( KEY, key );
+		super.setValue( NAME, name );
+		super.setValue( VALUE, value );
+		setModified( false );
+
+		definePrimaryKey( KEY, NAME, VALUE );
+		defineBusinessKey( KEY, NAME, VALUE );
+		defineReadOnly( KEY, NAME, VALUE );
+	}
+
+	public String getKey() {
+		return getValue( KEY );
+	}
+
+	public String getName() {
+		return getValue( NAME );
+	}
+
+	public String getOptionValue() {
+		return getValue( VALUE );
+	}
+
+	@Override
+	public String toString() {
+		return getName();
+	}
+
+}
---
+++ source/test/java/com/xeomar/xenon/tool/SettingOptionTest.java
@@ -0,0 +1,18 @@
+package com.xeomar.xenon.tool;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SettingOptionTest {
+
+	@Test
+	public void testConstructorWithKeyValueAnd() {
+		SettingOption option = new SettingOption( "info", "Information", "3" );
+		assertThat( option.getKey(), is( "info" ) );
+		assertThat( option.getName(), is( "Information" ) );
+		assertThat( option.getOptionValue(), is( "3" ) );
+	}
+
+}
