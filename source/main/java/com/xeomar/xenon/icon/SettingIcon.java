b7d552e7863a6d0f75d007519e1ba730fce9b345
Mark Soderquist <mark@soderquist.net>
2017-09-06 19:42:24 -0600

    Updated setting icon

--- source/main/java/com/xeomar/xenon/icon/SettingIcon.java
+++ source/main/java/com/xeomar/xenon/icon/SettingIcon.java
@@ -2,18 +2,27 @@ package com.xeomar.xenon.icon;
 
 import com.xeomar.xenon.ProgramIcon;
 
-public class SettingIcon  extends ProgramIcon {
+public class SettingIcon extends ProgramIcon {
 
 	@Override
 	protected void render() {
-		fillOval( g( 5 ), g( 11 ), g( 22 ), g( 10 ) );
+		//		beginPath();
+		//		addArc( g( 24 ), g( 16 ), g( 3 ), g( 3 ), 270, 180 );
+		//		lineTo( g( 8 ), g( 13 ) );
+		//		addArc( g( 8 ), g( 16 ), g( 3 ), g( 3 ), 90, 180 );
+		//		closePath();
+		//		fillAndDraw();
 
-		drawOval( g( 5 ), g( 11 ), g( 22 ), g( 10 ) );
+		beginPath();
+		addArc( g( 24 ), g( 16 ), g( 5 ), g( 5 ), 270, 180 );
+		lineTo( g( 8 ), g( 11 ) );
+		addArc( g( 8 ), g( 16 ), g( 5 ), g( 5 ), 90, 180 );
+		closePath();
+		fillAndDraw();
 	}
 
 	public static void main( String[] commands ) {
 		proof( new SettingIcon() );
 	}
-
 
 }
