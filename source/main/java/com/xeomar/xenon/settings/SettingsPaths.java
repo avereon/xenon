04fe139a627452dbd9f908f409f82786a2db60da
Mark Soderquist <mark@soderquist.net>
2017-10-05 20:51:38 -0600

    Continue work on Settings

--- source/main/java/com/xeomar/xenon/settings/SettingsPaths.java
+++ source/main/java/com/xeomar/xenon/settings/SettingsPaths.java
@@ -22,15 +22,31 @@ public class SettingsPaths {
 		return path != null && SEPARATOR.equals( path );
 	}
 
-	public static String path( String root, String path ) {
-		if( path != null && isAbsolute( path ) ) return path;
-		if( root == null ) return null;
-		if( path == null ) path = EMPTY;
-		return resolve( root + (root.endsWith( SEPARATOR ) ? EMPTY : SEPARATOR) + path );
+	/**
+	 * This method does not normalize the path.
+	 *
+	 * @param root
+	 * @param path
+	 * @return
+	 */
+	public static String resolve( String root, String path ) {
+		if( root == null || path == null ) return null;
+
+		if( EMPTY.equals( root ) ) return path;
+		if( EMPTY.equals( path ) ) return root;
+		if( isAbsolute( path ) ) return path;
+
+		return root + (root.endsWith( SEPARATOR ) ? EMPTY : SEPARATOR) + path;
 	}
 
+	/**
+	 * This method does not normalize the path.
+	 *
+	 * @param path
+	 * @return
+	 */
 	public static String getParent( String path ) {
-		path = cleanTrailingSeparator( collapseParentReferences( path ) );
+		path = cleanTrailingSeparator( path );
 		if( path == null ) return null;
 		if( isRoot( path ) ) return null;
 		if( EMPTY.equals( path ) ) return null;
@@ -40,9 +56,88 @@ public class SettingsPaths {
 		return path.substring( 0, index );
 	}
 
-	public static String resolve( String path ) {
+	public static String normalize( String path ) {
 		if( path == null ) return null;
-		return cleanTrailingSeparator( cleanMultiSeparators( collapseParentReferences( path ) ) );
+		return cleanTrailingSeparator( normalizeSeparators( normalizeParents( path ) ) );
+	}
+
+	public static String relativize( String source, String target ) {
+		if( source == null || target == null ) return null;
+
+		if( target.equals( source ) ) {
+			return EMPTY;
+		} else if( isAbsolute( source ) != isAbsolute( target )){
+			throw new IllegalArgumentException( "Target is different type of path" );
+		} else if( EMPTY.equals( source ) ) {
+			return target;
+		} else {
+			// NEXT Continue to implement relativize
+		}
+
+		return null;
+//		UnixPath var2 = toUnixPath(var1);
+//		if (var2.equals(this)) {
+//			return this.emptyPath();
+//		} else if (this.isAbsolute() != var2.isAbsolute()) {
+//			throw new IllegalArgumentException("'other' is different type of Path");
+//		} else if (this.isEmpty()) {
+//			return var2;
+//		} else {
+//			int var3 = this.getNameCount();
+//			int var4 = var2.getNameCount();
+//			int var5 = var3 > var4 ? var4 : var3;
+//
+//			int var6;
+//			for(var6 = 0; var6 < var5 && this.getName(var6).equals(var2.getName(var6)); ++var6) {
+//				;
+//			}
+//
+//			int var7 = var3 - var6;
+//			if (var6 < var4) {
+//				UnixPath var13 = var2.subpath(var6, var4);
+//				if (var7 == 0) {
+//					return var13;
+//				} else {
+//					boolean var14 = var2.isEmpty();
+//					int var10 = var7 * 3 + var13.path.length;
+//					if (var14) {
+//						assert var13.isEmpty();
+//
+//						--var10;
+//					}
+//
+//					byte[] var11 = new byte[var10];
+//
+//					int var12;
+//					for(var12 = 0; var7 > 0; --var7) {
+//						var11[var12++] = 46;
+//						var11[var12++] = 46;
+//						if (var14) {
+//							if (var7 > 1) {
+//								var11[var12++] = 47;
+//							}
+//						} else {
+//							var11[var12++] = 47;
+//						}
+//					}
+//
+//					System.arraycopy(var13.path, 0, var11, var12, var13.path.length);
+//					return new UnixPath(this.getFileSystem(), var11);
+//				}
+//			} else {
+//				byte[] var8 = new byte[var7 * 3 - 1];
+//
+//				for(int var9 = 0; var7 > 0; --var7) {
+//					var8[var9++] = 46;
+//					var8[var9++] = 46;
+//					if (var7 > 1) {
+//						var8[var9++] = 47;
+//					}
+//				}
+//
+//				return new UnixPath(this.getFileSystem(), var8);
+//			}
+//		}
 	}
 
 	private static String cleanTrailingSeparator( String path ) {
@@ -58,7 +153,7 @@ public class SettingsPaths {
 		return path;
 	}
 
-	private static String cleanMultiSeparators( String path ) {
+	private static String normalizeSeparators( String path ) {
 		if( path == null ) return null;
 		while( (path.indexOf( DOUBLE_SEPARATOR )) > -1 ) {
 			path = path.replace( DOUBLE_SEPARATOR, SEPARATOR );
@@ -66,7 +161,7 @@ public class SettingsPaths {
 		return path;
 	}
 
-	private static String collapseParentReferences( String path ) {
+	private static String normalizeParents( String path ) {
 		if( path == null ) return null;
 		String search = SEPARATOR + PARENT;
 		int searchLength = search.length();
--- source/test/java/com/xeomar/xenon/settings/SettingsPathsTest.java
+++ source/test/java/com/xeomar/xenon/settings/SettingsPathsTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.fail;
 
 public class SettingsPathsTest {
 
@@ -50,30 +51,91 @@ public class SettingsPathsTest {
 		assertThat( SettingsPaths.getParent( "/test/path" ), is( "/test" ) );
 		assertThat( SettingsPaths.getParent( "/test/path/" ), is( "/test" ) );
 
-		assertThat( SettingsPaths.getParent( "/../test" ), is( nullValue() ) );
+		assertThat( SettingsPaths.getParent( "/../test" ), is( "/.." ) );
+	}
+
+	@Test
+	public void testNormalize() {
+		// Normal paths
+		assertThat( SettingsPaths.normalize( null ), is( nullValue() ) );
+		assertThat( SettingsPaths.normalize( "" ), is( "" ) );
+		assertThat( SettingsPaths.normalize( "/" ), is( "/" ) );
+		assertThat( SettingsPaths.normalize( "/test" ), is( "/test" ) );
+		assertThat( SettingsPaths.normalize( "/test/path" ), is( "/test/path" ) );
+
+		// Trailing separators
+		assertThat( SettingsPaths.normalize( "/test/" ), is( "/test" ) );
+		assertThat( SettingsPaths.normalize( "/test/path/" ), is( "/test/path" ) );
+
+		// Multiple separators
+		assertThat( SettingsPaths.normalize( "/////test" ), is( "/test" ) );
+		assertThat( SettingsPaths.normalize( "/test/////path" ), is( "/test/path" ) );
+
+		// Parent references
+		assertThat( SettingsPaths.normalize( "/.." ), is( nullValue() ) );
+		assertThat( SettingsPaths.normalize( "/../test" ), is( nullValue() ) );
+		assertThat( SettingsPaths.normalize( "/test/../path" ), is( "/path" ) );
 	}
 
 	@Test
 	public void testResolve() {
+		// Null paths
+		assertThat( SettingsPaths.resolve( null, "" ), is( nullValue() ) );
+		assertThat( SettingsPaths.resolve( "", null ), is( nullValue() ) );
+
+		// Empty paths
+		assertThat( SettingsPaths.resolve( "", "" ), is( "" ) );
+		assertThat( SettingsPaths.resolve( "/", "" ), is( "/" ) );
+		assertThat( SettingsPaths.resolve( "", "/" ), is( "/" ) );
+		assertThat( SettingsPaths.resolve( "foo/bar", "" ), is( "foo/bar" ) );
+		assertThat( SettingsPaths.resolve( "", "foo/bar" ), is( "foo/bar" ) );
+
 		// Normal paths
-		assertThat( SettingsPaths.resolve( null ), is( nullValue() ) );
-		assertThat( SettingsPaths.resolve( "" ), is( "" ) );
-		assertThat( SettingsPaths.resolve( "/" ), is( "/" ) );
-		assertThat( SettingsPaths.resolve( "/test" ), is( "/test" ) );
-		assertThat( SettingsPaths.resolve( "/test/path" ), is( "/test/path" ) );
+		assertThat( SettingsPaths.resolve( "/", "foo" ), is( "/foo" ) );
+		assertThat( SettingsPaths.resolve( "/foo", "bar" ), is( "/foo/bar" ) );
+		assertThat( SettingsPaths.resolve( "/foo", "/bar" ), is( "/bar" ) );
+		assertThat( SettingsPaths.resolve( "/foo/bar", "gus" ), is( "/foo/bar/gus" ) );
+		assertThat( SettingsPaths.resolve( "/foo", "bar/gus" ), is( "/foo/bar/gus" ) );
+		assertThat( SettingsPaths.resolve( "foo", "bar" ), is( "foo/bar" ) );
+		assertThat( SettingsPaths.resolve( "foo", "/bar" ), is( "/bar" ) );
+		assertThat( SettingsPaths.resolve( "foo/bar", "gus" ), is( "foo/bar/gus" ) );
+		assertThat( SettingsPaths.resolve( "foo", "bar/gus" ), is( "foo/bar/gus" ) );
+	}
 
-		// Trailing separators
-		assertThat( SettingsPaths.resolve( "/test/" ), is( "/test" ) );
-		assertThat( SettingsPaths.resolve( "/test/path/" ), is( "/test/path" ) );
+	@Test
+	public void testRelativize() {
+		assertThat( SettingsPaths.relativize( null, "" ), is( nullValue() ) );
+		assertThat( SettingsPaths.relativize( "", null ), is( nullValue() ) );
 
-		// Multiple separators
-		assertThat( SettingsPaths.resolve( "/////test" ), is( "/test" ) );
-		assertThat( SettingsPaths.resolve( "/test/////path" ), is( "/test/path" ) );
+		// Equals source and target
+		assertThat( SettingsPaths.relativize( "/foo", "/foo" ), is( "" ) );
+		assertThat( SettingsPaths.relativize( "/foo/bar", "/foo/bar" ), is( "" ) );
 
-		// Parent references
-		assertThat( SettingsPaths.resolve( "/.." ), is( nullValue() ) );
-		assertThat( SettingsPaths.resolve( "/../test" ), is( nullValue() ) );
-		assertThat( SettingsPaths.resolve( "/test/../path" ), is( "/path" ) );
+		// Empty source
+		assertThat( SettingsPaths.relativize( "", "" ), is( "" ) );
+		assertThat( SettingsPaths.relativize( "", "foo" ), is( "foo" ) );
+		assertThat( SettingsPaths.relativize( "", "foo/bar" ), is( "foo/bar" ) );
+
+		// NEXT Continue to test relativize
+	}
+
+	@Test
+	public void testRelativizeWithMixedAbsoluteAndRelativePaths() {
+		// Absolute and relative
+		try {
+		SettingsPaths.relativize( "/foo", "bar" );
+			fail( "Expected an IllegalArgumentException to be thrown" );
+		} catch( IllegalArgumentException exception ) {
+			assertThat( exception.getMessage(), is( "Target is different type of path" ) );
+		}
+
+		// Relative and absolute
+		try {
+			SettingsPaths.relativize( "foo", "/bar" );
+			fail( "Expected an IllegalArgumentException to be thrown" );
+		} catch( IllegalArgumentException exception ) {
+			assertThat( exception.getMessage(), is( "Target is different type of path" ) );
+		}
 	}
 
 }
