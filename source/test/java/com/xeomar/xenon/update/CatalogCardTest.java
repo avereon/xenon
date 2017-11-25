f28701ea5dbfab8fc3e261e85cf54e560a6c745f
Mark Soderquist <mark@soderquist.net>
2017-11-24 19:03:22 -0700

    Fix unit test failure

--- source/test/java/com/xeomar/xenon/update/CatalogCardTest.java
+++ source/test/java/com/xeomar/xenon/update/CatalogCardTest.java
@@ -18,12 +18,12 @@ public class CatalogCardTest {
 	@Test
 	public void testReadCardFromYaml() throws Exception {
 		List<URI> products = new ArrayList<>();
-		products.add( URI.create( "http://www.xeomar.com/download/product/xenon/card" ) );
+		products.add( URI.create( "http://xeomar.com/download/product/xenon/card" ) );
 
 		InputStream input = getClass().getResourceAsStream( CatalogCard.CARD );
 		CatalogCard card = YmlUtil.load( input, CatalogCard.class );
 		assertThat( card.getName(), is( "Xeomar" ) );
-		assertThat( card.getIcon(), is( URI.create( "http://www.xeomar.com/download/image/xeomar.png" ) ) );
+		assertThat( card.getIcon(), is( URI.create( "http://xeomar.com/download/image/xeomar.png" ) ) );
 		assertThat( card.getProducts(), contains( products.toArray() ) );
 	}
 
