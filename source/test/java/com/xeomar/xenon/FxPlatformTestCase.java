bfcf3c1faabf24bc76feb949fe66b5ad262bce28
Mark Soderquist <mark@soderquist.net>
2018-04-02 21:08:59 -0600

    Change the platform test case implementation

--- source/test/java/com/xeomar/xenon/FxPlatformTestCase.java
+++ source/test/java/com/xeomar/xenon/FxPlatformTestCase.java
@@ -1,13 +1,13 @@
 package com.xeomar.xenon;
 
-import com.xeomar.xenon.util.JavaFxStarter;
 import org.junit.Before;
+import org.testfx.framework.junit.ApplicationTest;
 
-public abstract class FxPlatformTestCase extends BaseTestCase {
+public abstract class FxPlatformTestCase extends ApplicationTest {
 
 	@Before
 	public void setup() throws Exception {
-		JavaFxStarter.startAndWait( 5000 );
+		//JavaFxStarter.startAndWait( 5000 );
 	}
 
 }
