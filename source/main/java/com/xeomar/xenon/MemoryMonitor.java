a1e3e7c63a4e746d1eea0d8b5372a35a0b255391
Mark Soderquist <soderquistmv@ldschurch.org>
2018-06-28 13:12:37 -0600

    Continue work on memory monitor

--- source/main/java/com/xeomar/xenon/MemoryMonitor.java
+++ source/main/java/com/xeomar/xenon/MemoryMonitor.java
@@ -2,20 +2,44 @@ package com.xeomar.xenon;
 
 import com.xeomar.util.FileUtil;
 import javafx.application.Platform;
+import javafx.beans.value.WritableValue;
+import javafx.css.StyleableProperty;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
+import javafx.scene.AccessibleRole;
+import javafx.scene.Node;
 import javafx.scene.control.Label;
+import javafx.scene.control.Labeled;
+import javafx.scene.control.Skin;
+import javafx.scene.control.skin.LabelSkin;
+import javafx.scene.shape.Rectangle;
 
+import java.text.DecimalFormat;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.CopyOnWriteArraySet;
 
-public class MemoryMonitor extends Label {
+public class MemoryMonitor extends Labeled {
 
 	private static final String DIVIDER = "/";
 
+	private static Set<MemoryMonitor> monitors;
+
 	private static Timer timer;
 
-	private static Set<MemoryMonitor> monitors;
+	private static boolean showPercent;
+
+	private static DecimalFormat percentFormat = new DecimalFormat( "#0" );
+
+	private double allocatedPercent;
+
+	private double usedPercent;
+
+	private Rectangle memoryAllocated;
+
+	private Rectangle memoryUsed;
 
 	// IDEA updates every five seconds
 
@@ -34,9 +58,34 @@ public class MemoryMonitor extends Label {
 	}
 
 	public MemoryMonitor() {
-		//setMinHeight( 20 ); // 16 + 2px top and 2px bottom
-		setId( "status-bar-memory-monitor" );
+		initialize();
+	}
+
+	private void initialize() {
+		getStyleClass().setAll( "memory-monitor" );
+		setAccessibleRole( AccessibleRole.TEXT );
+		// MemoryMonitors are not focus traversable, unlike most other UI Controls.
+		// focusTraversable is styleable through css. Calling setFocusTraversable
+		// makes it look to css like the user set the value and css will not
+		// override. Initializing focusTraversable by calling set on the
+		// CssMetaData ensures that css will be able to override the value.
+		((StyleableProperty<Boolean>)(WritableValue<Boolean>)focusTraversableProperty()).applyStyle( null, Boolean.FALSE );
+
+		memoryAllocated = new Rectangle();
+		memoryAllocated.getStyleClass().add( "memory-monitor-allocated" );
+
+		memoryUsed = new Rectangle();
+		memoryUsed.getStyleClass().add( "memory-monitor-used" );
+
+		getChildren().addAll( memoryAllocated, memoryUsed );
+
 		monitors.add( this );
+	}
+
+	/** {@inheritDoc} */
+	@Override
+	protected Skin<?> createDefaultSkin() {
+		return new MemoryMonitorSkin( this );
 	}
 
 	public void close() {
@@ -56,20 +105,52 @@ public class MemoryMonitor extends Label {
 		String allocatedSize = FileUtil.getHumanSize( allocated );
 		String maximumSize = FileUtil.getHumanSize( maximum );
 
-		//		long usedMegabytes = used / MEGABYTE;
-		//		long allocatedMegabytes = allocated / MEGABYTE;
-		//		long maximumMegabytes = maximum / MEGABYTE;
-
 		String text;
-		//		if( MemoryMonitorFactory.isShowPercent() ) {
-		//			text = format.format( usedPercent * 100 ) + "% " + DIVIDER + " " + format.format( allocatedPercent * 100 ) + "% " + DIVIDER + " " + maximumSize ;
-		//		} else {
-		text = usedSize + " " + DIVIDER + " " + allocatedSize + " " + DIVIDER + " " + maximumSize;
-		//		}
+		if( isShowPercent() ) {
+			text = percentFormat.format( usedPercent * 100 ) + "% " + DIVIDER + " " + percentFormat.format( allocatedPercent * 100 ) + "% " + DIVIDER + " " + maximumSize;
+		} else {
+			text = usedSize + " " + DIVIDER + " " + allocatedSize + " " + DIVIDER + " " + maximumSize;
+		}
 
 		for( MemoryMonitor monitor : monitors ) {
-			Platform.runLater( () -> monitor.setText( text ) );
+			Platform.runLater( () -> monitor.update( usedPercent, allocatedPercent, text ) );
 		}
+	}
+
+	public static boolean isShowPercent() {
+		return showPercent;
+	}
+
+	public static void setShowPercent( boolean showPercent ) {
+		MemoryMonitor.showPercent = showPercent;
+	}
+
+	@Override
+	protected void layoutChildren() {
+		super.layoutChildren();
+
+		double width = super.getPrefWidth();
+		double height = super.getPrefHeight();
+
+		System.out.println( "Memory monitor: " + width + " x " + height );
+
+		memoryAllocated.setWidth( 400 );
+		memoryAllocated.setHeight( 20 );
+
+		memoryUsed.setWidth( 200 );
+		memoryUsed.setHeight( 20 );
+	}
+
+	@Override
+	protected void layoutInArea( Node child, double areaX, double areaY, double areaWidth, double areaHeight, double areaBaselineOffset, Insets margin, boolean fillWidth, boolean fillHeight, HPos halignment, VPos valignment ) {
+		super.layoutInArea( child, areaX, areaY, areaWidth, areaHeight, areaBaselineOffset, margin, fillWidth, fillHeight, halignment, valignment );
+	}
+
+	private void update( double used, double allocated, String text ) {
+		this.allocatedPercent = allocated;
+		this.usedPercent = used;
+		setText( text );
+		requestLayout();
 	}
 
 }
---
+++ source/main/java/com/xeomar/xenon/MemoryMonitorSkin.java
@@ -0,0 +1,11 @@
+package com.xeomar.xenon;
+
+import javafx.scene.control.Labeled;
+import javafx.scene.control.skin.LabeledSkinBase;
+
+public class MemoryMonitorSkin extends LabeledSkinBase {
+
+	public MemoryMonitorSkin( Labeled labeled ) {
+		super( labeled );
+	}
+}
