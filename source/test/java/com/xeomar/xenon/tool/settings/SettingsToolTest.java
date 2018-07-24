ca02541fe95cd8691d2f3e6a6f040cb15aae3924
Mark Soderquist <mark@soderquist.net>
2018-07-24 14:05:20 -0600

    Update settings tool test

--- source/test/java/com/xeomar/xenon/tool/settings/SettingsToolTest.java
+++ source/test/java/com/xeomar/xenon/tool/settings/SettingsToolTest.java
@@ -2,4 +2,11 @@ package com.xeomar.xenon.tool.settings;
 
 import com.xeomar.xenon.BaseToolTestCase;
 
-abstract class SettingsToolTest extends BaseToolTestCase {}
+abstract class SettingsToolTest extends BaseToolTestCase {
+
+	protected void openTool() {
+		clickOn( "#menu-program" );
+		clickOn( "#menuitem-settings" );
+	}
+
+}
