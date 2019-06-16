e70c9e0084742e7528cb62e074190fc769c9cce5
Mark Soderquist <mark@soderquist.net>
2019-06-15 18:54:33 -0600

    Updated power icon

--- source/main/java/com/xeomar/xenon/icon/PowerIcon.java
+++ source/main/java/com/xeomar/xenon/icon/PowerIcon.java
@@ -5,14 +5,57 @@ import javafx.scene.shape.ArcType;
 
 public class PowerIcon extends ProgramIcon {
 
+	private double angle = 35;
+
+	private double width = g( 4 );
+
+	private double radius = g( 12 );
+
+	private Point center = new Point( g(16), g(18) );
+
 	@Override
 	protected void render() {
-		double angle = 35;
-		double radius = g( 12 );
+		renderWithOutline();
+		//renderWithStroke();
+	}
 
-		setLineWidth( g(3) );
-		drawCenteredArc( g(16), g(18), radius, radius, 90 + angle, 360 - (2*angle), ArcType.OPEN );
-		drawLine( g(16), g(4), g(16), g(14) );
+	private void renderWithOutline() {
+		setFillPaint( getIconDrawColor() );
+
+		double halfWidth = 0.5 * width;
+		double outerRadius = radius + halfWidth;
+		double innerRadius = radius - halfWidth;
+
+		double alpha = RADIANS_PER_DEGREE * (90 - angle);
+
+		Point o = new Point( outerRadius * Math.cos( alpha ), outerRadius * Math.sin( alpha ) );
+		Point c = new Point( radius * Math.cos( alpha ), radius * Math.sin( alpha ) );
+
+		double extent = 360 - (2 * angle);
+		startPath();
+		moveTo( center.x - o.x, center.y - o.y );
+		addArc( center.x, center.y, outerRadius, outerRadius, 90 + angle, extent );
+		addArc( center.x + c.x, center.y - c.y, halfWidth, halfWidth, 90 - angle, 180 );
+		addArc( center.x, center.y, innerRadius, innerRadius, 90 - angle, -extent );
+		addArc( center.x - c.x, center.y - c.y, halfWidth, halfWidth, 270 + angle, 180 );
+		closePath();
+		fill();
+		//draw();
+
+		startPath();
+		moveTo( center.x + halfWidth, g( 4 ) );
+		addArc( center.x, g(4 ), halfWidth, halfWidth, 0, 180 );
+		lineTo( center.x - halfWidth, g(14) );
+		addArc( center.x, g(14), halfWidth, halfWidth, 180, 180 );
+		closePath();
+		fill();
+		//draw();
+	}
+
+	private void renderWithStroke() {
+		setLineWidth( width );
+		drawCenteredArc( center.x, center.y, radius, radius, 90 + angle, 360 - (2 * angle), ArcType.OPEN );
+		drawLine( g( 16 ), g( 4 ), g( 16 ), g( 14 ) );
 	}
 
 	public static void main( String[] commands ) {
