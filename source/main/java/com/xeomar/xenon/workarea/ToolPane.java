3b2e4a0582dfd7f61630784ede83669d0e7d49ed
Mark Soderquist <mark@soderquist.net>
2019-06-17 20:37:32 -0600

    Continue work on custom tool pane

--- source/main/java/com/xeomar/xenon/workarea/ToolPane.java
+++ source/main/java/com/xeomar/xenon/workarea/ToolPane.java
@@ -1,7 +1,10 @@
 package com.xeomar.xenon.workarea;
 
 import javafx.beans.property.SimpleListProperty;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
+import javafx.scene.control.SelectionModel;
+import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
@@ -13,9 +16,11 @@ public class ToolPane extends BorderPane {
 
 	private ToolHeader header;
 
-	//private CardPane;
+	private SelectionModel selectionModel;
 
 	public ToolPane() {
+		selectionModel = new ToolPaneSelectionModel( this );
+
 		// Create components
 		header = new ToolHeader();
 
@@ -25,6 +30,10 @@ public class ToolPane extends BorderPane {
 
 	public ObservableList<ToolTab> getTabs() {
 		return header.getTabs();
+	}
+
+	public SelectionModel<ToolTab> getSelectionModel() {
+		return selectionModel;
 	}
 
 	void setTool( Tool tool ) {
@@ -43,6 +52,59 @@ public class ToolPane extends BorderPane {
 
 		ObservableList<ToolTab> getTabs() {
 			return tabs;
+		}
+
+	}
+
+	private static class ToolPaneSelectionModel extends SingleSelectionModel<ToolTab> {
+
+		private ToolPane pane;
+
+		ToolPaneSelectionModel( ToolPane pane ) {
+			if( pane == null ) throw new NullPointerException( "ToolPane can not be null" );
+
+			this.pane = pane;
+
+			ListChangeListener<ToolTab> listener = ( change ) -> {
+				while( change.next() ) {
+					for( ToolTab tab : change.getRemoved() ) {
+						if( tab != null && !pane.getTabs().contains( tab ) && tab.isSelected() ) {
+							tab.setSelected( false );
+							int fromIndex = change.getFrom();
+							this.findNearestAvailableTab( fromIndex, true );
+						}
+					}
+
+					if( (change.wasAdded() || change.wasRemoved()) && this.getSelectedIndex() != pane.getTabs().indexOf( this.getSelectedItem() ) ) {
+						this.clearAndSelect( pane.getTabs().indexOf( this.getSelectedItem() ) );
+					}
+				}
+
+				if( this.getSelectedIndex() == -1 && this.getSelectedItem() == null && pane.getTabs().size() > 0 ) {
+					this.findNearestAvailableTab( 0, true );
+				} else if( pane.getTabs().isEmpty() ) {
+					this.clearSelection();
+				}
+			};
+
+			if( pane.getTabs() != null ) pane.getTabs().addListener( listener );
+		}
+
+		@Override
+		protected ToolTab getModelItem( int index ) {
+			ObservableList<ToolTab> list = pane.getTabs();
+			if( list == null ) return null;
+			return index >= 0 && index < list.size() ? list.get( index ) : null;
+		}
+
+		@Override
+		protected int getItemCount() {
+			ObservableList<ToolTab> list = pane.getTabs();
+			return list == null ? 0 : list.size();
+		}
+
+		void findNearestAvailableTab( int index, boolean what ) {
+			// TODO Implement findNearestAvailableTab()
 		}
 
 	}
--- source/main/java/com/xeomar/xenon/workarea/ToolTab.java
+++ source/main/java/com/xeomar/xenon/workarea/ToolTab.java
@@ -1,7 +1,39 @@
 package com.xeomar.xenon.workarea;
 
+import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.value.ObservableBooleanValue;
 import javafx.scene.control.Labeled;
 
 public class ToolTab extends Labeled {
+
+	private Tool tool;
+
+	private BooleanProperty selected;
+
+	public ToolTab( Tool tool) {
+		this.tool = tool;
+		this.selected = new SimpleBooleanProperty( this, "selected" );
+	}
+
+	public ObservableBooleanValue selectedProperty() {
+		return selected;
+	}
+
+	public Boolean isSelected() {
+		return selected.get();
+	}
+
+	public void setSelected( Boolean selected ) {
+		this.selected.set( selected );
+	}
+
+	public Tool getContent() {
+		return getTool();
+	}
+
+	public Tool getTool() {
+		return tool;
+	}
 
 }
