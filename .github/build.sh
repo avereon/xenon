07d6a628e9b97dd4b76fe51695c43d55544d20f3
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-09-13 07:52:57 -0600

    Only use Xvfb on Linux

--- .github/build.sh
+++ .github/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-export DISPLAY=:99
-Xvfb ${DISPLAY} -screen 0 1920x1080x24 -nolisten unix &
+if [ "${MATRIX_OS}" == "ubuntu-latest" ]; then
+  export DISPLAY=:99
+  Xvfb ${DISPLAY} -screen 0 1920x1080x24 -nolisten unix &
+fi
+
 mvn deploy -B -U -V -P testui,platform-specific-assemblies --settings .github/settings.xml --file pom.xml
