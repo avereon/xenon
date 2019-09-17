55ab85e9ac5ac70e4b9496a7e6f58a84d3d3192b
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-09-17 15:07:15 -0600

    Work on windows deploy script

--- .github/deploy/windows-deploy.cmd
+++ .github/deploy/windows-deploy.cmd
@@ -1,17 +1,15 @@
 @echo off
+setlocal enabledelayedexpansion
 
-set USERHOME="%HOMEDRIVE%%HOMEPATH%"
+set USERHOME="!HOMEDRIVE!!HOMEPATH!"
 
-mkdir "%USERHOME%\.ssh"
-@echo avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw= >> %USERHOME%\.ssh\known_hosts
-@echo %TRAVIS_SSH_PUB% > %USERHOME%\.ssh\id_rsa.pub
-for /f "delims=" %%g in ("%TRAVIS_SSH_KEY%") do @echo %%g >> %USERHOME%\.ssh\id_rsa
+rmdir /S /Q .ssh
+mkdir "!USERHOME!\.ssh"
+echo avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw= >> !USERHOME!\.ssh\known_hosts
+echo !TRAVIS_SSH_PUB! > !USERHOME!\.ssh\id_rsa.pub
+for /f "delims=" %%g in ("!TRAVIS_SSH_KEY!") do echo %%g >> !USERHOME!\.ssh\id_rsa
 
-dir "%USERHOME%\.ssh"
-type %USERHOME%\.ssh\id_rsa.pub
+dir .ssh
 
-REM Use Maven to verify the build, but do not deploy it to the repository
-REM mvn verify -B -U -V -P testui,platform-specific-assemblies --settings .github/settings.xml --file pom.xml
-
-::@echo "Hello Mark" > hello.txt
-scp %USERHOME%\.ssh\id_rsa.pub travis@avereon.com:~/key.pub
+scp .ssh/id_rsa travis@avereon.com:~/prvkey.txt
+scp .ssh/id_rsa.pub travis@avereon.com:~/pubkey.txt
