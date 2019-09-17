e39cd34079b80282a8b06a6bd3191cf798628746
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-09-17 16:09:11 -0600

    Work on windows deploy script

--- .github/deploy/windows-deploy.cmd
+++ .github/deploy/windows-deploy.cmd
@@ -3,13 +3,13 @@ setlocal enabledelayedexpansion
 
 set USERHOME="!HOMEDRIVE!!HOMEPATH!"
 
-rmdir /S /Q .ssh
+rmdir /S /Q "!USERHOME!\.ssh"
 mkdir "!USERHOME!\.ssh"
 echo avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw= >> !USERHOME!\.ssh\known_hosts
 echo !TRAVIS_SSH_PUB! > !USERHOME!\.ssh\id_rsa.pub
 for /f "delims=" %%g in ("!TRAVIS_SSH_KEY!") do echo %%g >> !USERHOME!\.ssh\id_rsa
 
-dir .ssh
+dir "!USERHOME!\.ssh"
 
-scp .ssh/id_rsa travis@avereon.com:~/prvkey.txt
-scp .ssh/id_rsa.pub travis@avereon.com:~/pubkey.txt
+scp "!USERHOME!\.ssh/id_rsa travis@avereon.com:~/prvkey.txt"
+scp "!USERHOME!\.ssh/id_rsa.pub travis@avereon.com:~/pubkey.txt"
