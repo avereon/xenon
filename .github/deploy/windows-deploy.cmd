c68476bfb56a33fdd152015891591862d5ac1c09
Mark Soderquist <mark@soderquist.net>
2019-09-16 19:58:31 -0600

    Work on windows deploy script

--- .github/deploy/windows-deploy.cmd
+++ .github/deploy/windows-deploy.cmd
@@ -1,14 +1,14 @@
 @echo off
 
-set HOME="%HOMEDRIVE%%HOMEPATH%"
+set USERHOME="%HOMEDRIVE%%HOMEPATH%"
 
-mkdir "%HOME%\.ssh"
-@echo "" > %HOME%\.ssh\id_rsa
-for %%f in (%TRAVIS_SSH_KEY%) do @echo %%f >> %HOME%\.ssh\id_rsa
-@echo "%TRAVIS_SSH_PUB%" > %HOME%\.ssh\id_rsa.pub
-@echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> %HOME%\.ssh\known_hosts
+mkdir "%USERHOME%\.ssh"
+@echo "" > %USERHOME%\.ssh\id_rsa
+for %%f in (%TRAVIS_SSH_KEY%) do @echo %%f >> %USERHOME%\.ssh\id_rsa
+@echo "%TRAVIS_SSH_PUB%" > %USERHOME%\.ssh\id_rsa.pub
+@echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> %USERHOME%\.ssh\known_hosts
 
-dir "%HOME%\.ssh"
+dir "%USERHOME%\.ssh"
 
 REM Use Maven to verify the build, but do not deploy it to the repository
 REM mvn verify -B -U -V -P testui,platform-specific-assemblies --settings .github/settings.xml --file pom.xml
