92fe73f596d1743778906445032029cd2d8c5beb
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-09-17 09:25:32 -0600

    Work on windows deploy script

--- .github/deploy/windows-deploy.cmd
+++ .github/deploy/windows-deploy.cmd
@@ -12,5 +12,5 @@ dir "%USERHOME%\.ssh"
 REM Use Maven to verify the build, but do not deploy it to the repository
 REM mvn verify -B -U -V -P testui,platform-specific-assemblies --settings .github/settings.xml --file pom.xml
 
-@echo Hello Mark > hello.txt
+@echo "Hello Mark" > hello.txt
 scp hello.txt travis@avereon.com:~
