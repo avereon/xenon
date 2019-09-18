10ec57faf2ee3e6830fb1e05669ccd839a3e6ad7
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-09-17 19:00:40 -0600

    Work on windows deploy script

--- .github/deploy/windows-deploy.cmd
+++ .github/deploy/windows-deploy.cmd
@@ -7,7 +7,7 @@ set "USERHOME=."
 
 rmdir /S /Q "!USERHOME!\.ssh"
 mkdir "!USERHOME!\.ssh"
-echo avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw= > !USERHOME!\.ssh\known_hosts
+echo avereon.com,159.65.110.114 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw= > !USERHOME!\.ssh\known_hosts
 echo !TRAVIS_SSH_PUB! > !USERHOME!\.ssh\id_rsa.pub
 for /f "delims=" %%g in ("!TRAVIS_SSH_KEY!") do echo %%g >> "!USERHOME!\.ssh\id_rsa"
 
