741d75784e931d78c9f9addee5237522261e55ca
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-09-17 18:57:13 -0600

    Work on windows deploy script

--- .github/deploy/windows-deploy.cmd
+++ .github/deploy/windows-deploy.cmd
@@ -7,12 +7,14 @@ set "USERHOME=."
 
 rmdir /S /Q "!USERHOME!\.ssh"
 mkdir "!USERHOME!\.ssh"
-echo avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw= >> !USERHOME!\.ssh\known_hosts
+echo avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw= > !USERHOME!\.ssh\known_hosts
 echo !TRAVIS_SSH_PUB! > !USERHOME!\.ssh\id_rsa.pub
 for /f "delims=" %%g in ("!TRAVIS_SSH_KEY!") do echo %%g >> "!USERHOME!\.ssh\id_rsa"
 
 dir "!USERHOME!\.ssh"
-md5sum "!USERHOME!\.ssh\*sc "
+md5sum "!USERHOME!\.ssh\id_rsa"
+md5sum "!USERHOME!\.ssh\id_rsa.pub"
+type "!USERHOME!\.ssh\known_hosts"
 
 scp -i "!USERHOME!\.ssh\id_rsa" "!USERHOME!\.ssh\id_rsa" travis@avereon.com:~/prvkey.txt
 scp -i "!USERHOME!\.ssh\id_rsa" "!USERHOME!\.ssh\id_rsa.pub" travis@avereon.com:~/pubkey.txt
