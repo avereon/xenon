bb944753baba7c803e5e3f069c3d856128703444
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-10-03 14:48:24 -0600

    Try switching to gpg encrypted files

--- .github/deploy/linux-deploy.sh
+++ .github/deploy/linux-deploy.sh
@@ -23,9 +23,14 @@ echo "[github.ref]=${GITHUB_REF}"
 echo "Deploy path=/opt/avn/store/$RELEASE/$PRODUCT/$PLATFORM"
 
 mkdir ${HOME}/.ssh
-echo "${TRAVIS_SSH_KEY}" > ${HOME}/.ssh/id_rsa
-echo "${TRAVIS_SSH_PUB}" > ${HOME}/.ssh/id_rsa.pub
-echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> ${HOME}/.ssh/known_hosts
+#echo "${TRAVIS_SSH_KEY}" > ${HOME}/.ssh/id_rsa
+#echo "${TRAVIS_SSH_PUB}" > ${HOME}/.ssh/id_rsa.pub
+#echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> ${HOME}/.ssh/known_hosts
+
+gpg --quiet --batch --yes --decrypt --passphrase="$AVN_KEYSTORE_PASSWORD" --output $HOME/.ssh/id_rsa .github/deploy/id_rsa.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSWORD" --output $HOME/.ssh/id_rsa.pub .github/deploy/id_rsa.pub.gpg
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSWORD" --output $HOME/.ssh/known_hosts .github/deploy/known_hosts.gpg
+
 chmod 600 ${HOME}/.ssh/id_rsa
 chmod 600 ${HOME}/.ssh/id_rsa.pub
 chmod 600 ${HOME}/.ssh/known_hosts
