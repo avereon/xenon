e515834e6858d63377bd5310d340fcd74417de35
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2019-10-03 16:16:09 -0600

    #82 Sign jar files

--- .github/deploy/linux-deploy.sh
+++ .github/deploy/linux-deploy.sh
@@ -15,27 +15,33 @@ if [ "${PLATFORM}" == "linux" ]; then
   Xvfb ${DISPLAY} -screen 0 1920x1080x24 -nolisten unix &
 fi
 
-gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output .github/avereon.keystore .github/avereon.keystore.gpg
-
-rm -rf target/jlink
-mvn verify -B -U -V -P testui,platform-specific-assemblies --settings .github/settings.xml --file pom.xml
-
 echo "Build date=$(date)"
 echo "[github.ref]=${GITHUB_REF}"
 echo "Deploy path=/opt/avn/store/$RELEASE/$PRODUCT/$PLATFORM"
 
 mkdir ${HOME}/.ssh
+gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOME/.ssh/id_rsa .github/id_rsa.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOME/.ssh/id_rsa.pub .github/id_rsa.pub.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOME/.ssh/known_hosts .github/known_hosts.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output .github/avereon.keystore .github/avereon.keystore.gpg
 #echo "${TRAVIS_SSH_KEY}" > ${HOME}/.ssh/id_rsa
 #echo "${TRAVIS_SSH_PUB}" > ${HOME}/.ssh/id_rsa.pub
 #echo 'avereon.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw=' >> ${HOME}/.ssh/known_hosts
 
-gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOME/.ssh/id_rsa .github/id_rsa.gpg
-gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOME/.ssh/id_rsa.pub .github/id_rsa.pub.gpg
-gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOME/.ssh/known_hosts .github/known_hosts.gpg
-
 chmod 600 ${HOME}/.ssh/id_rsa
 chmod 600 ${HOME}/.ssh/id_rsa.pub
 chmod 600 ${HOME}/.ssh/known_hosts
+
+ls -al $HOME/.ssh
+sha1sum $HOME/.ssh/id_rsa
+sha1sum $HOME/.ssh/id_rsa.pub
+sha1sum $HOME/.ssh/known_hosts
+
+# Test sending before building
+scp -B source/main/resources/ascii-art-title.txt travis@avereon.com:/opt/avn/store/$RELEASE/$PRODUCT/$PLATFORM
+
+rm -rf target/jlink
+mvn verify -B -U -V -P testui,platform-specific-assemblies --settings .github/settings.xml --file pom.xml
 
 scp -B target/*install.jar travis@avereon.com:/opt/avn/store/$RELEASE/$PRODUCT/$PLATFORM
 scp -B target/*product.jar travis@avereon.com:/opt/avn/store/$RELEASE/$PRODUCT/$PLATFORM
--- .github/deploy/windows-deploy.cmd
+++ .github/deploy/windows-deploy.cmd
@@ -15,11 +15,6 @@ echo "Build timestamp=!DATEVAL! !TIMEVAL!"
 echo "[github.ref]=!GITHUB_REF!"
 echo "Deploy path=/opt/avn/store/!RELEASE!/!PRODUCT!/!PLATFORM!"
 
-cmd /c gpg --quiet --batch --yes --decrypt --passphrase=!AVN_GPG_PASSWORD! --output .github\avereon.keystore .github\avereon.keystore.gpg
-
-rmdir /S /Q target\jlink
-cmd /c mvn verify -B -U -V -P testui,platform-specific-assemblies --settings .github/settings.xml --file pom.xml
-
 rmdir /S /Q "!SSHHOME!"
 mkdir "!SSHHOME!"
 echo avereon.com,159.65.110.114 ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBAX0k5tSvrXVpKl7HNPIPglp6Kyj0Ypty6M3hgR783ViTzhRnojEZvdCXuYiGSVKEzZWr9oYQnLr03qjU/t0SNw= >> !SSHHOME!\known_hosts
@@ -27,10 +22,18 @@ echo.>> !SSHHOME!\known_hosts
 echo !TRAVIS_SSH_PUB! > !SSHHOME!\id_rsa.pub
 for /f "delims=" %%g in ("!TRAVIS_SSH_KEY!") do echo %%g >> "!SSHHOME!\id_rsa"
 
+cmd /c gpg --quiet --batch --yes --decrypt --passphrase=!AVN_GPG_PASSWORD! --output .github\avereon.keystore .github\avereon.keystore.gpg
+
 dir "!SSHHOME!"
 sha1sum "!SSHHOME!\id_rsa"
 sha1sum "!SSHHOME!\id_rsa.pub"
 sha1sum "!SSHHOME!\known_hosts"
+
+# Test sending before building
+scp -B source/main/resources/ascii-art-title.txt travis@avereon.com:/opt/avn/store/!RELEASE!/!PRODUCT!/!PLATFORM!
+
+rmdir /S /Q target\jlink
+cmd /c mvn verify -B -U -V -P testui,platform-specific-assemblies --settings .github/settings.xml --file pom.xml
 
 scp -B target/install.jar travis@avereon.com:/opt/avn/store/!RELEASE!/!PRODUCT!/!PLATFORM!
 scp -B target/product.jar travis@avereon.com:/opt/avn/store/!RELEASE!/!PRODUCT!/!PLATFORM!
