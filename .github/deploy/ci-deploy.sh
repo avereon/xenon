099d2f9a515a8f8e963a654556301ef442d4a7de
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2020-01-16 14:45:17 -0700

    Simplify the CI pipelines

--- .github/deploy/ci-deploy.sh
+++ .github/deploy/ci-deploy.sh
@@ -5,10 +5,10 @@ PRODUCT='xenon'
 DEPLOY_PATH="/opt/avn/store/$RELEASE/$PRODUCT"
 
 #RELEASE github.ref [refs/heads/master, refs/heads/stable]
-case "${GITHUB_REF}" in
-  "refs/heads/master") RELEASE="latest" ;;
-  "refs/heads/stable") RELEASE="stable" ;;
-esac
+#case "${GITHUB_REF}" in
+#  "refs/heads/master") RELEASE="latest" ;;
+#  "refs/heads/stable") RELEASE="stable" ;;
+#esac
 
 echo "Build date=$(date)"
 echo "[github.ref]=${GITHUB_REF}"
@@ -23,10 +23,10 @@ chmod 600 "${HOME}/.ssh/id_rsa"
 chmod 600 "${HOME}/.ssh/id_rsa.pub"
 chmod 600 "${HOME}/.ssh/known_hosts"
 
-ls -al "$HOME/.ssh"
-sha1sum "$HOME/.ssh/id_rsa"
-sha1sum "$HOME/.ssh/id_rsa.pub"
-sha1sum "$HOME/.ssh/known_hosts"
+#ls -al "$HOME/.ssh"
+#sha1sum "$HOME/.ssh/id_rsa"
+#sha1sum "$HOME/.ssh/id_rsa.pub"
+#sha1sum "$HOME/.ssh/known_hosts"
 
 JAVADOC="/opt/avn/web/product/xenon/javadoc"
 scp -B target/xenon-*-javadoc.jar travis@avereon.com:$DEPLOY_PATH/javadoc.jar 2>&1
