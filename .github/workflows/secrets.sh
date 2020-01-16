78afb9d52deced345a7f25aa056f1b12068d720e
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2020-01-16 15:19:15 -0700

    Simplify the CI pipelines

---
+++ .github/workflows/secrets.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir "${HOME}/.ssh"
+gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output .github/avereon.keystore .github/avereon.keystore.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOME/.ssh/id_rsa .github/id_rsa.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOME/.ssh/id_rsa.pub .github/id_rsa.pub.gpg
+gpg --quiet --batch --yes --decrypt --passphrase=$AVN_GPG_PASSWORD --output $HOME/.ssh/known_hosts .github/known_hosts.gpg
+chmod 600 "${HOME}/.ssh/id_rsa"
+chmod 600 "${HOME}/.ssh/id_rsa.pub"
+chmod 600 "${HOME}/.ssh/known_hosts"
