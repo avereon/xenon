0d5f80738c8bbc3d6014741331bfa545cd5ac23e
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2020-02-12 07:56:34 -0700

    Improve event watcher logging

--- source/main/java/com/avereon/xenon/ProgramEventWatcher.java
+++ source/main/java/com/avereon/xenon/ProgramEventWatcher.java
@@ -2,24 +2,47 @@ package com.avereon.xenon;
 
 import com.avereon.event.Event;
 import com.avereon.event.EventHandler;
+import com.avereon.event.EventType;
 import com.avereon.util.Log;
+import com.avereon.xenon.task.TaskEvent;
 
 import java.lang.System.Logger;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProgramEventWatcher implements EventHandler<Event> {
 
 	private static final Logger log = Log.log();
 
+	private static final Map<EventType<?>, Logger.Level> LEVELS;
+
+	private static final Logger.Level DEFAULT_LEVEL = Log.TRACE;
+
+	static {
+		LEVELS = new HashMap<>();
+		LEVELS.put( ProgramEvent.STARTING, Log.DEBUG );
+		LEVELS.put( ProgramEvent.STARTED, Log.INFO );
+		LEVELS.put( ProgramEvent.STOPPING, Log.DEBUG );
+		LEVELS.put( ProgramEvent.STOPPED, Log.INFO );
+		LEVELS.put( TaskEvent.PROGRESS, Log.TRACE );
+
+		//LEVELS.put( AssetEvent.ANY, Log.WARN );
+	}
+
 	public void handle( Event event ) {
-		if( "PROGRESS".equals( event.getEventType().getName() ) ) {
-			log.log( Log.TRACE,  String.valueOf( event ) );
-		} else if( event instanceof ProgramEvent ) {
-			log.log( Log.INFO,  String.valueOf( event ) );
-//		} else if( event instanceof AssetEvent ) {
-//			log.log( Log.WARN,  String.valueOf( event ) );
-		} else {
-			log.log( Log.DEBUG,  String.valueOf( event ) );
+		log.log( getLevel( event ), String.valueOf( event ) );
+	}
+
+	private Logger.Level getLevel( Event event ) {
+		EventType<?> type = event.getEventType();
+
+		while( type != null ) {
+			Logger.Level level = LEVELS.get( type );
+			if( level != null ) return level;
+			type = type.getParentEventType();
 		}
+
+		return DEFAULT_LEVEL;
 	}
 
 }
