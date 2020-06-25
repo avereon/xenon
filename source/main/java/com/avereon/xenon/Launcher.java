d7646f6edb732d8ac2e44c2b2b2b778d37dba4f2
Mark Soderquist <mark@soderquist.net>
2020-06-24 20:09:57 -0600

    Continue work on new update process

--- source/main/java/com/avereon/xenon/Launcher.java
+++ source/main/java/com/avereon/xenon/Launcher.java
@@ -8,10 +8,8 @@ public class Launcher {
 		com.avereon.util.Parameters parameters = com.avereon.util.Parameters.parse( commands );
 		if( parameters.isSet( ElevatedFlag.CALLBACK_SECRET ) ) {
 			new com.avereon.zenna.Program().start( commands );
-			System.exit( 0 );
 		} else if( parameters.isSet( ProgramFlag.UPDATE ) ) {
 			new com.avereon.zenna.Program().start( new Program().updateProgram( parameters ) );
-			System.exit( 0 );
 		} else {
 			Program.launch( commands );
 		}
