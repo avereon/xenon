c2b7b7d3f1eb3a0aa3bd65458e79c6063af393b5
Mark Soderquist <mark@soderquist.net>
2020-06-28 20:55:56 -0600

    Programmer notes

--- source/main/java/com/avereon/xenon/Launcher.java
+++ source/main/java/com/avereon/xenon/Launcher.java
@@ -8,6 +8,7 @@ public class Launcher {
 	public static void main( String[] commands ) {
 		com.avereon.util.Parameters parameters = com.avereon.util.Parameters.parse( commands );
 		if( parameters.isSet( ElevatedFlag.CALLBACK_SECRET ) ) {
+			// FIXME In order for the elevated launcher to work the name of the launcher must be configured
 			new com.avereon.zenna.Program().start( commands );
 		} else if( parameters.isSet( UpdateFlag.UPDATE ) ) {
 			new com.avereon.zenna.Program().start( commands );
