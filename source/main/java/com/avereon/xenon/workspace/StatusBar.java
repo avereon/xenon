3efe61cbc2399320acf60a0f28c5354641fc6b96
Mark Soderquist <soderquistmv@churchofjesuschrist.org>
2020-10-14 15:15:26 -0600

    Enhance status bar

--- source/main/java/com/avereon/xenon/workspace/StatusBar.java
+++ source/main/java/com/avereon/xenon/workspace/StatusBar.java
@@ -4,6 +4,8 @@ import javafx.scene.Node;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 
+import java.util.Arrays;
+
 public class StatusBar extends BorderPane {
 
 	private HBox leftStatusBarItems;
@@ -23,16 +25,26 @@ public class StatusBar extends BorderPane {
 		setRight( rightStatusBarItems );
 	}
 
-	public void addLeft( Node node ) {
-		if( !leftStatusBarItems.getChildren().contains( node ) ) leftStatusBarItems.getChildren().add( node );
+	public void setLeft( Node... nodes ) {
+		leftStatusBarItems.getChildren().clear();
+		addLeft( nodes );
+	}
+
+	public void addLeft( Node... nodes ) {
+		Arrays.stream( nodes ).filter( n -> !leftStatusBarItems.getChildren().contains( n ) ).forEach( n -> leftStatusBarItems.getChildren().add( n ) );
 	}
 
 	public void removeLeft( Node node ) {
 		leftStatusBarItems.getChildren().remove( node );
 	}
 
-	public void addRight( Node node ) {
-		if( !rightStatusBarItems.getChildren().contains( node ) ) rightStatusBarItems.getChildren().add( 0, node );
+	public void setRight( Node... nodes ) {
+		rightStatusBarItems.getChildren().clear();
+		addRight( nodes );
+	}
+
+	public void addRight( Node... nodes ) {
+		Arrays.stream( nodes ).filter( n -> !rightStatusBarItems.getChildren().contains( n ) ).forEach( n -> rightStatusBarItems.getChildren().add( n ) );
 	}
 
 	public void removeRight( Node node ) {
