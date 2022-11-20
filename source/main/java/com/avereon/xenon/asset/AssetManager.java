32dcddff85bc5c5dd9bbc64c822e2cc250ee3c05
msoderquist <msoderquist@mastercontrol.com>
2022-11-19 21:29:12 -0700

    Work on help documents

--- source/main/java/com/avereon/xenon/asset/AssetManager.java
+++ source/main/java/com/avereon/xenon/asset/AssetManager.java
@@ -1111,7 +1111,8 @@ public class AssetManager implements Controllable<AssetManager> {
 
 		// It's problematic to check if an asset exists, particularly for new assets
 		//if( !asset.exists() ) return false;
-		if( !asset.getScheme().canLoad( asset )) return false;
+
+		if( !asset.getScheme().canLoad( asset ) ) return false;
 
 		if( !asset.getScheme().canLoad( asset ) ) return false;
 
