229383af6c153d142b7ca64e94b01ec9ea743af7
msoderquist <msoderquist@mastercontrol.com>
2023-05-17 17:58:53 -0600

    Fix ProgramProduct inheritance

--- source/main/java/com/avereon/xenon/XenonProgramProduct.java
+++ source/main/java/com/avereon/xenon/XenonProgramProduct.java
@@ -1,8 +1,8 @@
 package com.avereon.xenon;
 
-import com.avereon.product.Product;
+import com.avereon.product.ProgramProduct;
 
-public interface XenonProgramProduct extends Product {
+public interface XenonProgramProduct extends ProgramProduct {
 
 	Xenon getProgram();
 
