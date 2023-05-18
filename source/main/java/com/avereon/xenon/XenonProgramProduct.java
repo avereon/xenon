905538642ec51ffd35e08c31775c404262a33c5e
msoderquist <msoderquist@mastercontrol.com>
2023-05-17 18:15:54 -0600

    Fix ProgramProduct inheritance

--- source/main/java/com/avereon/xenon/XenonProgramProduct.java
+++ source/main/java/com/avereon/xenon/XenonProgramProduct.java
@@ -1,9 +1,15 @@
 package com.avereon.xenon;
 
 import com.avereon.product.ProgramProduct;
+import com.avereon.xenon.task.Task;
+
+import java.util.concurrent.Callable;
 
 public interface XenonProgramProduct extends ProgramProduct {
 
 	Xenon getProgram();
 
+	default <T> void task( String name, Callable<T> callable ) {
+		getProgram().getTaskManager().submit( Task.of( name, callable ) );
+	}
 }
