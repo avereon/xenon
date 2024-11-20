7e05b5fce14f85c4aadb6ef7ef76e85f9ec93945
msoderquist <msoderquist@mastercontrol.com>
2024-11-20 15:57:46 -0700

    Test more NewScheme methods

--- source/test/java/com/avereon/xenon/scheme/NewSchemeTest.java
+++ source/test/java/com/avereon/xenon/scheme/NewSchemeTest.java
@@ -2,22 +2,86 @@ package com.avereon.xenon.scheme;
 
 import com.avereon.xenon.BasePartXenonTestCase;
 import com.avereon.xenon.asset.Asset;
+import com.avereon.xenon.asset.AssetType;
+import com.avereon.xenon.asset.Codec;
 import com.avereon.xenon.asset.MockAssetType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.*;
 
+@ExtendWith( MockitoExtension.class )
 public class NewSchemeTest extends BasePartXenonTestCase {
 
 	private NewScheme scheme;
+
+	@Mock
+	private Codec codec;
+
+	@Mock
+	private AssetType assetType;
+
+	private Asset asset;
 
 	@BeforeEach
 	protected void setup() throws Exception {
 		super.setup();
 		scheme = new NewScheme( getProgram() );
+		asset = new Asset( assetType, NewScheme.uri() );
+
+		lenient().when( assetType.getDefaultCodec() ).thenReturn( codec );
+	}
+
+	@Test
+	void canLoad() throws Exception {
+		assertThat( scheme.canLoad( asset ) ).isTrue();
+	}
+
+	@Test
+	void canSave() throws Exception {
+		assertThat( scheme.canSave( asset ) ).isTrue();
+	}
+
+	@Test
+	void load() throws Exception {
+		// given
+		Path path = scheme.getTemporaryPath( asset );
+		Files.createDirectories( path.getParent() );
+		Files.createFile( path );
+		path.toFile().deleteOnExit();
+
+		// when
+		scheme.load( asset, codec );
+
+		// then
+		verify( codec, times( 1 ) ).load( eq( asset ), any( InputStream.class ) );
+	}
+
+	@Test
+	void loadWithoutContent() throws Exception {
+		// when
+		scheme.load( asset, codec );
+
+		// then
+		verify( codec, times( 0 ) ).load( eq( asset ), any( InputStream.class ) );
+	}
+
+	@Test
+	void save() throws Exception {
+		// when
+		scheme.save( asset, codec );
+
+		// then
+		verify( codec, times( 1 ) ).save( eq( asset ), any( OutputStream.class ) );
 	}
 
 	@Test
