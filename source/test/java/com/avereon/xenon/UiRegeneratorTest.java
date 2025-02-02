9b3443e2b9612763ab2a9f1c5d705f0b109e5c88
msoderquist <msoderquist@mastercontrol.com>
2025-02-02 10:59:48 -0700

    Working on UI resources

--- source/test/java/com/avereon/xenon/UiRegeneratorTest.java
+++ source/test/java/com/avereon/xenon/UiRegeneratorTest.java
@@ -3,7 +3,6 @@ package com.avereon.xenon;
 import com.avereon.util.ThreadUtil;
 import com.avereon.zarra.javafx.Fx;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -31,7 +30,7 @@ class UiRegeneratorTest extends BaseFullXenonTestCase {
 	}
 
 	@Test
-	@Disabled
+//	@Disabled
 	void createDefaultWorkspace() {
 		// given
 		Path settingsFolder = getProgram().getDataFolder().resolve( SettingsManager.ROOT );
@@ -39,7 +38,7 @@ class UiRegeneratorTest extends BaseFullXenonTestCase {
 		assertThat( uiSettingsFolder ).doesNotExist();
 
 //		// NEXT Add a listener to the UI settings to watch when everything is saved
-//		// FIXME Apparently settings events are not bubbled up to parents?
+//		// NOTE Apparently settings events are not bubbled up to parents?
 //		Settings uiSettings = getProgram().getSettingsManager().getSettings(ProgramSettings.UI);
 //		uiSettings.register( SettingsEvent.ANY, e -> {
 //			System.out.println( "Settings saved: " + e.getEventType() + " " + e.getPath() );
@@ -50,11 +49,17 @@ class UiRegeneratorTest extends BaseFullXenonTestCase {
 		// when
 		Fx.run( () -> regenerator.createDefaultWorkspace() );
 		// NOTE I'm thinking that I have to wait long enough for all the settings to be saved
-		// That's 500ms plus a bit more for good measure, and that's too long
-		ThreadUtil.pause( 1000 );
+		// That's 500ms plus a bit more for good measure, and that's awfully long
+		ThreadUtil.pause( 3000 );
 
 		// Check the settings folder for the expected files
 		assertThat( uiSettingsFolder ).exists();
+		assertThat( uiSettingsFolder.resolve( "area" ) ).exists();
+		//assertThat( uiSettingsFolder.resolve( "edge" ) ).exists();
+		assertThat( uiSettingsFolder.resolve( "pane" ) ).exists();
+		//assertThat( uiSettingsFolder.resolve( "tool" ) ).exists();
+		//assertThat( uiSettingsFolder.resolve( "view" ) ).exists();
+		assertThat( uiSettingsFolder.resolve( "workspace" ) ).exists();
 	}
 
 }
