e2d3b23b305d5951dab3abb45e8280e4c6f294ae
msoderquist <msoderquist@mastercontrol.com>
2025-05-11 20:17:12 -0600

    Refactor ProductUpdate class to use Lombok for getters and setters

--- source/main/java/com/avereon/xenon/product/ProductUpdate.java
+++ source/main/java/com/avereon/xenon/product/ProductUpdate.java
@@ -1,13 +1,11 @@
 package com.avereon.xenon.product;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.avereon.product.ProductCard;
 import com.avereon.product.RepoCard;
-import com.avereon.settings.Settings;
-import com.avereon.xenon.Configurable;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 /**
  * NOTE: This class is Persistent and changing the package will most likely
@@ -15,7 +13,9 @@ import java.nio.file.Paths;
  *
  * @author SoderquistMV
  */
-public final class ProductUpdate implements Configurable {
+@Setter
+@Getter
+public final class ProductUpdate {
 
 	private ProductCard card;
 
@@ -24,9 +24,6 @@ public final class ProductUpdate implements Configurable {
 	private Path source;
 
 	private Path target;
-
-	@JsonIgnore
-	private Settings settings;
 
 	/*
 	 * This constructor is used by the settings API via reflection.
@@ -40,52 +37,6 @@ public final class ProductUpdate implements Configurable {
 		this.target = target;
 	}
 
-	public ProductCard getCard() {
-		return card;
-	}
-
-	public void setCard( ProductCard card ) {
-		this.card = card;
-	}
-
-	public RepoCard getRepo() {
-		return repo;
-	}
-
-	public void setRepo( RepoCard repo ) {
-		this.repo = repo;
-	}
-
-	public Path getSource() {
-		return source;
-	}
-
-	public void setSource( Path source ) {
-		this.source = source;
-	}
-
-	public Path getTarget() {
-		return target;
-	}
-
-	public void setTarget( Path target ) {
-		this.target = target;
-	}
-
-	@Override
-	public void setSettings( Settings settings ) {
-		this.settings = settings;
-		String sourcePath = settings.get( "source" );
-		String targetPath = settings.get( "target" );
-		source = sourcePath == null ? null : Paths.get( sourcePath );
-		target = targetPath == null ? null : Paths.get( targetPath );
-	}
-
-	@Override
-	public Settings getSettings() {
-		return settings;
-	}
-
 	@Override
 	public int hashCode() {
 		return card.hashCode();
@@ -93,8 +44,7 @@ public final class ProductUpdate implements Configurable {
 
 	@Override
 	public boolean equals( Object object ) {
-		if( !(object instanceof ProductUpdate) ) return false;
-		ProductUpdate that = (ProductUpdate)object;
+		if( !(object instanceof ProductUpdate that) ) return false;
 		return this.card.equals( that.card );
 	}
 
