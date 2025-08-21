fef3f676836ad79c97eb0ae62a502e8eb015d432
Mark Soderquist <mark@soderquist.net>
2025-08-20 18:59:29 -0600

    Disable `AboutToolOpenTwiceUIT.execute` test on macOS.

--- source/test/java/com/avereon/xenon/tool/AboutToolOpenTwiceUIT.java
+++ source/test/java/com/avereon/xenon/tool/AboutToolOpenTwiceUIT.java
@@ -3,12 +3,15 @@ package com.avereon.xenon.tool;
 import com.avereon.xenon.workpane.ToolEvent;
 import com.avereon.xenon.workpane.Workpane;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AboutToolOpenTwiceUIT extends AboutToolUIT {
 
 	@Test
+	@DisabledOnOs( OS.MAC )
 	void execute() throws Exception {
 		Workpane pane = getWorkarea();
 		assertToolCount( pane, 0 );
