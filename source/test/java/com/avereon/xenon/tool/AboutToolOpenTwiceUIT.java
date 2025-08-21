abc869fd88a4483d89c70b704430bdee7b694bc0
Mark Soderquist <mark@soderquist.net>
2025-08-20 19:43:03 -0600

    Add logging control to tests and update comments in AboutToolOpenTwiceUIT
    - Introduced `setLogLevel` method in `BaseForAllTests` for test-specific logging control.
    - Adjusted log level within `AboutToolOpenTwiceUIT.execute` for better debugging on macOS.
    - Updated and clarified comments regarding intermittent macOS test failures.

--- source/test/java/com/avereon/xenon/tool/AboutToolOpenTwiceUIT.java
+++ source/test/java/com/avereon/xenon/tool/AboutToolOpenTwiceUIT.java
@@ -1,29 +1,34 @@
 package com.avereon.xenon.tool;
 
+import com.avereon.log.LogLevel;
 import com.avereon.xenon.workpane.ToolEvent;
 import com.avereon.xenon.workpane.Workpane;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.OS;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class AboutToolOpenTwiceUIT extends AboutToolUIT {
 
 	@Test
-	@DisabledOnOs( OS.MAC )
-		// This appears to be failing on macOS, but for an unknown reason
-		// Other tests of this nature appear to be passing on macOS.
 	void execute() throws Exception {
+
 		Workpane pane = getWorkarea();
 		assertToolCount( pane, 0 );
 
+		// NOTE This appears to be intermittently failing on macOS but for an unknown reason.
+		// Other tests of this nature also fail occasionally on macOS.
+
+		// DEGUG This line appears to execute correctly by causing the about tool to be added to the workpane.
+		// However, the guide is not added to the workpane. This is a similar problem with other guided tools,
+		// when they happen to fail.
+		setLogLevel( LogLevel.INFO );
 		openAboutTool();
 		getWorkpaneEventWatcher().waitForEvent( ToolEvent.ADDED );
 		getWorkpaneEventWatcher().waitForEvent( ToolEvent.ADDED );
 
 		assertThat( pane.getActiveTool() ).isInstanceOf( AboutTool.class );
 		assertToolCount( pane, 2 );
+		setLogLevel( LogLevel.OFF );
 
 		// Try to open the tool again and make sure there is still only one
 		openAboutTool();
