62d78f2fb2fa854bafcf94701767f34b7e9f0181
Mark Soderquist <mark@soderquist.net>
2025-08-21 22:07:37 -0600

    Simplify `BaseToolUIT` by removing redundant setup method and streamlining assertions
    - Removed the `@BeforeEach` setup method, eliminating unused imports and redundant tool registration checks.
    - Simplified `assertToolCount` by removing unnecessary logging and direct assertions.

--- source/test/java/com/avereon/xenon/BaseToolUIT.java
+++ source/test/java/com/avereon/xenon/BaseToolUIT.java
@@ -1,13 +1,6 @@
 package com.avereon.xenon;
 
-import com.avereon.xenon.asset.AssetType;
-import com.avereon.xenon.asset.type.ProgramGuideType;
-import com.avereon.xenon.workpane.Tool;
 import com.avereon.xenon.workpane.Workpane;
-import org.junit.jupiter.api.BeforeEach;
-
-import java.util.Collection;
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,30 +8,8 @@ public abstract class BaseToolUIT extends BaseXenonUIT {
 
 	protected static final String MAIN_MENU = "#menu-button-menu";
 
-	@BeforeEach
-	protected void setup() throws Exception {
-		super.setup();
-
-		// This seems to be a problem on MacOS for some reason
-		AssetType assetType = getProgram().getAssetManager().getAssetType( ProgramGuideType.URI.toString() );
-		assertThat( assetType ).isNotNull();
-
-		// No tools registered for asset type xenon:/guide
-		List<Class<? extends ProgramTool>> tools = getProgram().getToolManager().getRegisteredTools( assetType );
-		assertThat( tools ).isNotNull();
-		assertThat( tools ).isNotEmpty();
-	}
-
 	protected void assertToolCount( Workpane pane, int count ) {
-		Collection<Tool> tools = pane.getTools();
-		assertThat( tools ).isNotNull();
-
-		try {
-			assertThat( tools ).hasSize( count );
-		} catch( AssertionError error ) {
-			tools.forEach( t -> System.out.println( "Tool: " + t ) );
-			throw error;
-		}
+		assertThat( pane.getTools() ).hasSize( count );
 	}
 
 	protected void openMenuItem( String menuId, String menuItemId ) {
