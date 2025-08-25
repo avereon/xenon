fb883d2512a4e30a0dfae34f53be89693fc1069b
Mark Soderquist <mark@soderquist.net>
2025-08-25 09:47:47 -0600

    Programmer notes

--- source/test/java/com/avereon/xenon/BaseToolUIT.java
+++ source/test/java/com/avereon/xenon/BaseToolUIT.java
@@ -33,6 +33,20 @@ public abstract class BaseToolUIT extends BaseXenonUIT {
 		// first of this type of test to be run, but others fail on occasion as well:
 		// - AboutToolCloseAssetCloseToolUIT
 		//
+ 		// The JvmSureStop timeout is 10 seconds. I have adjusted this time to 15
+		// seconds on 24 Aug 2025 to see if that helps. If it does, then I might
+		// consider increasing the timeout to 20 seconds, just to give more
+		// breathing room for the tests. If not, at least we have a lead about what
+		// is going on. Result: this does not appear to have been the cause.
+		//
+ 		// This might also be a mix of the Maven FailSafe plugin not using a
+		// separate JVM for each UI test as expected, new tests being run in an old
+		// JVM that is shutting down, and the JvmSureStop hook terminating the JVM
+		// as expected. Xenon tries to allow the JVM to terminate nicely, but we
+		// implemented JvmSureStop to ensure that the JVM does terminate. There may
+		// be something going on with the tests (not really a surprise) that is
+		// causing the JVM to not terminate.
+		//
 		// 24 Aug 2025 - Finally some progress
 		// We finally have some evidence of premature shutdown. See the following
 		// output. Also note the time different between the workspace visible event
@@ -77,20 +91,6 @@ public abstract class BaseToolUIT extends BaseXenonUIT {
 		//		Type  xenon:/settings
 		//		Type  xenon:/task
 		//		Type  xenon:/guide
-		//
- 		// The JvmSureStop timeout is 10 seconds. I have adjusted this time to 15
-		// seconds on 24 Aug 2025 to see if that helps. If it does, then I might
-		// consider increasing the timeout to 20 seconds, just to give more
-		// breathing room for the tests. If not, at least we have a lead about what
-		// is going on. Result: this does not appear to have been the cause.
-		//
- 		// This might also be a mix of the Maven FailSafe plugin not using a
-		// separate JVM for each UI test as expected, new tests being run in an old
-		// JVM that is shutting down, and the JvmSureStop hook terminating the JVM
-		// as expected. Xenon tries to allow the JVM to terminate nicely, but we
-		// implemented JvmSureStop to ensure that the JVM does terminate. There may
-		// be something going on with the tests (not really a surprise) that is
-		// causing the JVM to not terminate.
 	}
 
 	protected void assertToolCount( Workpane pane, int count ) {
