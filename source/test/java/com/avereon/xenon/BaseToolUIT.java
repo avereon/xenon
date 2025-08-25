f67f6735fbde4d5af4b7d264db72adf63790b0fb
Mark Soderquist <mark@soderquist.net>
2025-08-24 20:36:44 -0600

    Expand debug information in `BaseToolUIT` regarding premature shutdown and tool registration issues.

--- source/test/java/com/avereon/xenon/BaseToolUIT.java
+++ source/test/java/com/avereon/xenon/BaseToolUIT.java
@@ -22,44 +22,75 @@ public abstract class BaseToolUIT extends BaseXenonUIT {
 		// consistent failure, even during the same test run and most times, only
 		// one or two tests will fail due to this problem.
 		//
- 		// We have not, however, determined what is causing the tool registrations
+		// We have not, however, determined what is causing the tool registrations
 		// not to be complete. We have investigated incomplete setup as well as
 		// premature teardown, and neither has resulted in evidence of a problem.
 		//
- 		// We have researched the possibility of multiple asset or tool managers and
+		// We have researched the possibility of multiple asset or tool managers and
 		// believe we have ruled out this situation.
 		//
 		// The test that fails most often appears to be, possibly because it is the
 		// first of this type of test to be run, but others fail on occasion as well:
 		// - AboutToolCloseAssetCloseToolUIT
-
-		//		// Test output
-		//		//		2025-08-22 03:55:02.832 [W] c.a.x.ToolManager.determineToolClassForAssetType: No tools registered for asset type xenon:/guide
-		//		//		Error:  Tests run: 1, Failures: 1, Errors: 0, Skipped: 0, Time elapsed: 4.685 s <<< FAILURE! -- in com.avereon.xenon.tool.product.ProductToolCloseAssetCloseToolUIT
 		//
-		//		// Not enough tools registered
-		//    // Not only is xenon:/guide not registered, but so many other asset types do not have registrations either
-		/// /		2025-08-22 16:52:14.037 [W] c.a.x.ToolManager.determineToolClassForAssetType: No tools registered for asset type xenon:/guide
-		/// /		2025-08-22 16:52:14.038 [W] c.a.x.ToolManager.determineToolClassForAssetType: Using tool manager: 514793058
-		/// /		2025-08-22 16:52:14.038 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/index-search
-		/// /		2025-08-22 16:52:14.038 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/help
-		/// /		2025-08-22 16:52:14.038 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/themes
-		/// /		2025-08-22 16:52:14.038 [W] c.a.x.ToolManager.determineToolClassForAssetType:   Tool  ThemeTool
-		/// /		2025-08-22 16:52:14.038 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/notice
-		/// /		2025-08-22 16:52:14.038 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/asset
-		/// /		2025-08-22 16:52:14.039 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/modules
-		/// /		2025-08-22 16:52:14.039 [W] c.a.x.ToolManager.determineToolClassForAssetType:   Tool  SettingsTool
-		/// /		2025-08-22 16:52:14.039 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  mock
-		/// /		2025-08-22 16:52:14.039 [W] c.a.x.ToolManager.determineToolClassForAssetType:   Tool  MockGuidedTool
-		/// /		2025-08-22 16:52:14.039 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/about
-		/// /		2025-08-22 16:52:14.039 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/task
-		/// /		2025-08-22 16:52:14.039 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/fault
-		/// /		2025-08-22 16:52:14.039 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/new
-		/// /		2025-08-22 16:52:14.040 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/welcome
-		/// /		2025-08-22 16:52:14.040 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/properties
-		/// /		2025-08-22 16:52:14.040 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/settings
-		/// /		2025-08-22 16:52:14.040 [W] c.a.x.ToolManager.determineToolClassForAssetType: Type  xenon:/guide
-
+		// 24 Aug 2025 - Finally some progress
+		// We finally have some evidence of premature shutdown. See the following
+		// output. Also note the time different between the workspace visible event
+		// and the program shutdown event, about 10 seconds. Is there a test timeout
+		// getting in the way?:
+		//		Program start duration=1007
+		//		time=2389 marker=program-started thread=TaskPool-3-worker-1
+		//		time=2692 marker=workspace-visible thread=JavaFX Application Thread
+		//		registerTool: assetType=mock -> tool=com.avereon.xenon.tool.guide.GuidedToolUIT$MockGuidedTool
+		//		time=3640 marker=program-shutdown thread=JavaFX Application Thread
+		//		unregisterTool: assetType=xenon:/properties -> tool=com.avereon.xenon.tool.PropertiesTool
+		//		time=3641 marker=do-shutdown-tasks thread=TaskPool-1-worker-1
+		//		2025-08-25 01:48:01.804 [W] c.a.x.ToolManager.determineToolClassForAssetType: No tools registered for asset type xenon:/guide
+		//		unregisterTool: assetType=xenon:/help -> tool=com.avereon.xenon.tool.HelpTool
+		//		unregisterTool: assetType=xenon:/asset -> tool=com.avereon.xenon.tool.AssetTool
+		//		unregisterTool: assetType=xenon:/new -> tool=com.avereon.xenon.tool.NewAssetTool
+		//		unregisterTool: assetType=xenon:/fault -> tool=com.avereon.xenon.tool.FaultTool
+		//		unregisterTool: assetType=xenon:/task -> tool=com.avereon.xenon.tool.TaskTool
+		//		unregisterTool: assetType=xenon:/modules -> tool=com.avereon.xenon.tool.product.ProductTool
+		//		unregisterTool: assetType=xenon:/welcome -> tool=com.avereon.xenon.tool.WelcomeTool
+		//		unregisterTool: assetType=xenon:/index-search -> tool=com.avereon.xenon.tool.SearchTool
+		//		unregisterTool: assetType=xenon:/notice -> tool=com.avereon.xenon.tool.NoticeTool
+		//		unregisterTool: assetType=xenon:/settings -> tool=com.avereon.xenon.tool.settings.SettingsTool
+		//		unregisterTool: assetType=xenon:/about -> tool=com.avereon.xenon.tool.AboutTool
+		//		unregisterTool: assetType=xenon:/guide -> tool=com.avereon.xenon.tool.guide.GuideTool
+		//		ToolManager
+		//		Type  xenon:/fault
+		//		Type  xenon:/themes
+		//		Tool  ThemeTool
+		//		Type  xenon:/modules
+		//		Tool  SettingsTool
+		//		Type  xenon:/help
+		//		Type  xenon:/properties
+		//		Type  xenon:/about
+		//		Type  xenon:/welcome
+		//		Type  xenon:/index-search
+		//		Type  xenon:/new
+		//			Type  mock
+		//		Tool  MockGuidedTool
+		//		Type  xenon:/asset
+		//		Type  xenon:/notice
+		//		Type  xenon:/settings
+		//		Type  xenon:/task
+		//		Type  xenon:/guide
+		//
+ 		// The JvmSureStop timeout is 10 seconds. I have adjusted this time to 15
+		// seconds on 24 Aug 2025 to see if that helps. If it does, then I might
+		// consider increasing the timeout to 20 seconds, just to give more
+		// breathing room for the tests. If not, at least we have a lead about what
+		// is going on. Result: this does not appear to have been the cause.
+		//
+ 		// This might also be a mix of the Maven FailSafe plugin not using a
+		// separate JVM for each UI test as expected, new tests being run in an old
+		// JVM that is shutting down, and the JvmSureStop hook terminating the JVM
+		// as expected. Xenon tries to allow the JVM to terminate nicely, but we
+		// implemented JvmSureStop to ensure that the JVM does terminate. There may
+		// be something going on with the tests (not really a surprise) that is
+		// causing the JVM to not terminate.
 	}
 
 	protected void assertToolCount( Workpane pane, int count ) {
