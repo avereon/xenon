5eb36dadcd03e71c4a1e4cb30b18349197b86069
Mark Soderquist <mark@soderquist.net>
2025-09-08 16:04:27 -0600

    Enhance `ToolEvent` toString method for detailed throwable output
    - Include throwable stack trace in string representation using `StringWriter` and `PrintWriter`.

--- source/main/java/com/avereon/xenon/workpane/ToolEvent.java
+++ source/main/java/com/avereon/xenon/workpane/ToolEvent.java
@@ -4,7 +4,9 @@ import com.avereon.util.Jvm;
 import javafx.event.EventType;
 import lombok.Getter;
 
+import java.io.PrintWriter;
 import java.io.Serial;
+import java.io.StringWriter;
 
 @Getter
 public class ToolEvent extends WorkpaneEvent {
@@ -59,7 +61,12 @@ public class ToolEvent extends WorkpaneEvent {
 		long eventId = System.identityHashCode( this ) | Jvm.ID;
 		long toolId = System.identityHashCode( getSource() ) | Jvm.ID;
 		StringBuilder builder = new StringBuilder( "ToolEvent[" + eventId + "]" + getEventType() + " for tool=[" + toolId + "]" );
-		if( throwable != null ) builder.append( "\n  with throwable=" ).append( throwable );
+		if( throwable != null ) {
+			StringWriter writer = new StringWriter();
+			throwable.printStackTrace( new PrintWriter( writer ) );
+			builder.append( "\n  with throwable=" ).append( throwable );
+			builder.append( "\n" ).append( writer );
+		}
 		return builder.toString();
 	}
 
