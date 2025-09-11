e7648b64d1f57da864fa7139684bf8dc61c692b8
Mark Soderquist <mark@soderquist.net>
2025-09-11 12:14:54 -0600

    Add helper method to resolve and apply skins to JavaFX controls
    - Introduce `resolve` method to ensure skins are applied by displaying controls in a temporary stage.
    - Add `@AfterEach` cleanup to close the stage after each test.
    - Import required JavaFX classes and update `BaseFxPlatformTestCase` setup accordingly.

--- source/test/java/com/avereon/xenon/BaseFxPlatformTestCase.java
+++ source/test/java/com/avereon/xenon/BaseFxPlatformTestCase.java
@@ -1,9 +1,18 @@
 package com.avereon.xenon;
 
 import com.avereon.zerra.javafx.Fx;
+import javafx.scene.Scene;
+import javafx.scene.control.Control;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
+import static com.avereon.xenon.test.ProgramTestConfig.TIMEOUT;
+
 public abstract class BaseFxPlatformTestCase extends BaseForAllTests {
+
+	private Stage stage;
 
 	@BeforeEach
 	protected void setup() throws Exception {
@@ -13,6 +22,27 @@ public abstract class BaseFxPlatformTestCase extends BaseForAllTests {
 
 		// Start the FX platform
 		Fx.startup();
+	}
+
+	@AfterEach
+	void after() {
+		Fx.run( () -> stage.close() );
+	}
+
+	// This method is needed in order for skins to be applied to controls
+	protected <T extends Control> T resolve( T control ) {
+		Scene scene = new Scene( control );
+
+		Fx.run( () -> {
+			stage = new Stage( StageStyle.UNDECORATED );
+			stage.setScene( scene );
+			// Yes, we actually have to show the stage to get the skin applied
+			stage.show();
+			stage.toBack();
+		} );
+
+		Fx.waitFor( TIMEOUT );
+		return control;
 	}
 
 }
